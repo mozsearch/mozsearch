d1f2fcb05da1b4c095ba2391b50b9e2b3072e098
Bill McCloskey <billm@mozilla.com>
2016-01-16 21:14:29 -0800

    Fix output-dir script

--- scripts/output.sh
+++ scripts/output.sh
@@ -13,9 +13,8 @@ cat $INDEX_ROOT/repo-files $INDEX_ROOT/objdir-files | grep "$FILTER" | \
     parallel --halt 2 -X --eta \
     $JS $MOZSEARCH_ROOT/output-file.js $TREE_ROOT $INDEX_ROOT $MOZSEARCH_ROOT $OBJDIR
 
-cat $INDEX_ROOT/repo-dirs $INDEX_ROOT/objdir-dirs | grep "$FILTER" | \
-    parallel --halt 2 -X --eta \
-    $JS $MOZSEARCH_ROOT/output-dir.js $TREE_ROOT $INDEX_ROOT $MOZSEARCH_ROOT $OBJDIR
+cat $INDEX_ROOT/repo-files $INDEX_ROOT/objdir-files | grep "$FILTER" > /tmp/dirs
+$JS $MOZSEARCH_ROOT/output-dir.js $TREE_ROOT $INDEX_ROOT $MOZSEARCH_ROOT $OBJDIR /tmp/dirs
 
 $JS $MOZSEARCH_ROOT/output-template.js $TREE_ROOT $INDEX_ROOT $MOZSEARCH_ROOT
 $JS $MOZSEARCH_ROOT/output-help.js $TREE_ROOT $INDEX_ROOT $MOZSEARCH_ROOT
