44716753e386c1b6c70a58292ddb188dcb777a58
Bill McCloskey <billm@mozilla.com>
2016-05-01 15:11:25 -0700

    Move some rust files around; add initial output code

--- tools/src/bin/crossref.rs
+++ tools/src/bin/crossref.rs
@@ -6,37 +6,11 @@ use std::io::Write;
 use std::collections::HashMap;
 use std::collections::BTreeMap;
 
+extern crate tools;
+use tools::{read_analysis, read_target, source_path, AnalysisKind};
+
 extern crate rustc_serialize;
-use rustc_serialize::json::{Json, Object, ToJson};
-
-#[derive(Eq, PartialEq, PartialOrd, Ord, Debug)]
-struct Location {
-    lineno: u32,
-    col_start: u32,
-    col_end: u32,
-}
-
-#[derive(Debug)]
-struct WithLocation<T> {
-    data: T,
-    loc: Location,
-}
-
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
-enum AnalysisKind {
-    Use,
-    Def,
-    Assign,
-    Decl,
-    Idl,
-}
-
-#[derive(Debug)]
-struct AnalysisTarget {
-    kind: AnalysisKind,
-    pretty: String,
-    sym: String,
-}
+use rustc_serialize::json::{Json, ToJson};
 
 #[derive(Debug, RustcEncodable, RustcDecodable)]
 struct SearchResult {
@@ -51,108 +25,6 @@ impl ToJson for SearchResult {
         obj.insert("line".to_string(), self.line.to_json());
         Json::Object(obj)
     }
-}
-
-/*
-struct AnalysisSource {
-    loc: String,
-    pretty: String,
-    sym: String,
-}
-*/
-
-/*
-struct AnalysisIterator<T, LinesIter> {
-    filter: &Fn(Json) -> Option<T>,
-    lines: LinesIter,
-}
- */
-
-fn parse_location(loc: &str) -> Location {
-    let v : Vec<&str> = loc.split(":").collect();
-    let lineno = v[0].parse::<u32>().unwrap();
-    let (col_start, col_end) = if v[1].contains("-") {
-        let v : Vec<&str> = v[1].split("-").collect();
-        (v[0], v[1])
-    } else {
-        (v[1], v[1])
-    };
-    let col_start = col_start.parse::<u32>().unwrap();
-    let col_end = col_end.parse::<u32>().unwrap();
-    Location { lineno: lineno, col_start: col_start, col_end: col_end }
-}
-
-fn read_analysis<T>(filename: &str, filter: &Fn(&Object) -> Option<T>) -> Vec<WithLocation<Vec<T>>> {
-    let f = File::open(filename).unwrap();
-    let file = BufReader::new(&f);
-    let mut result = Vec::new();
-    for line in file.lines() {
-        let data = Json::from_str(&line.unwrap()).unwrap();
-        let obj = data.as_object().unwrap();
-        let loc = parse_location(obj.get("loc").unwrap().as_string().unwrap());
-        match filter(obj) {
-            Some(v) => result.push(WithLocation { data: v, loc: loc }),
-            None => {}
-        }
-    }
-
-    result.sort_by(|x1, x2| {
-        x1.loc.cmp(&x2.loc)
-    });
-
-    let mut result2 = Vec::new();
-    let mut last_loc = None;
-    let mut last_vec = Vec::new();
-    for r in result {
-        match last_loc {
-            Some(ll) => {
-                if ll == r.loc {
-                    last_loc = Some(ll);
-                } else {
-                    result2.push(WithLocation { loc: ll, data: last_vec });
-                    last_vec = Vec::new();
-                    last_loc = Some(r.loc);
-                }
-            },
-            None => {
-                last_loc = Some(r.loc);
-            }
-        }
-        last_vec.push(r.data);
-    }
-
-    result2
-}
-
-fn read_target(obj : &Object) -> Option<AnalysisTarget> {
-    if !obj.contains_key("target") {
-        return None;
-    }
-
-    let kindstr = obj.get("kind").unwrap().as_string().unwrap();
-    let kind = match kindstr {
-        "use" => AnalysisKind::Use,
-        "def" => AnalysisKind::Def,
-        "assign" => AnalysisKind::Assign,
-        "decl" => AnalysisKind::Decl,
-        "idl" => AnalysisKind::Idl,
-        _ => panic!("bad target kind")
-    };
-
-    let pretty = match obj.get("pretty") {
-        Some(json) => json.as_string().unwrap().to_string(),
-        None => "".to_string()
-    };
-    let sym = obj.get("sym").unwrap().as_string().unwrap().to_string();
-
-    Some(AnalysisTarget { kind: kind, pretty: pretty, sym: sym })
-}
-
-fn source_path(path: &str, tree_root: &str, objdir: &str) -> String {
-    if path.starts_with("__GENERATED__") {
-        return path.replace("__GENERATED__", objdir);
-    }
-    (tree_root.to_string() + "/").to_string() + path
 }
 
 fn main() {
