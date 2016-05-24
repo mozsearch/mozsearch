aee41735c5881256ee52eab75c6e69a0727bfaf4
Bill McCloskey <billm@mozilla.com>
2016-05-23 19:51:02 -0700

    Typo fix

--- aws/indexer-startup.sh
+++ aws/indexer-startup.sh
@@ -227,7 +227,7 @@ $VENV/bin/python $AWS_ROOT/detach-volume.py $EC2_INSTANCE_ID $VOLUME_ID
 $VENV/bin/python $AWS_ROOT/swap-web-server.py $CHANNEL $EC2_INSTANCE_ID $VOLUME_ID
 
 gzip -k ~ubuntu/index-log
-$VENV/bin/pythin $AWS_ROOT/upload.py ~ubuntu/index-log.gz indexer-logs `date -Iminutes`
+$VENV/bin/python $AWS_ROOT/upload.py ~ubuntu/index-log.gz indexer-logs `date -Iminutes`
 
 # Give logger time to catch up
 sleep 30
