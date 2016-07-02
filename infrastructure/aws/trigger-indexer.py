c5a0084d198fb24d7727f3191352a915bc9ae08c
Bill McCloskey <billm@mozilla.com>
2016-07-02 13:36:06 -0700

    more fixes

--- infrastructure/aws/trigger-indexer.py
+++ infrastructure/aws/trigger-indexer.py
@@ -13,25 +13,25 @@ config_repo = sys.argv[1]
 # 'release' or 'dev'
 channel = sys.argv[2]
 
-userData = '''#!/bin/bash
+user_data = '''#!/bin/bash
 
 cd ~ubuntu
 ./update.sh "{channel}" "{config_repo}"
 sudo -i -u ubuntu mozsearch/infrastructure/aws/index.sh "{channel}" "{config_repo}" config
 '''.format(channel=channel, config_repo=config_repo)
 
-blockDevices = []
+block_devices = []
 
 images = client.describe_images(Filters=[{'Name': 'name', 'Values': ['indexer']}])
 image_id = images['Images'][0]['ImageId']
 
-launchSpec = {
+launch_spec = {
     'ImageId': image_id,
     'KeyName': 'Main Key Pair',
     'SecurityGroups': ['indexer'],
-    'UserData': userData,
+    'UserData': user_data,
     'InstanceType': 'c3.2xlarge',
-    'BlockDeviceMappings': blockDevices,
+    'BlockDeviceMappings': block_devices,
     'IamInstanceProfile': {
         'Name': 'indexer-role',
     },
@@ -47,7 +47,7 @@ if spot:
         InstanceCount=1,
         Type='one-time',
         BlockDurationMinutes=300,
-        LaunchSpecification=launchSpec,
+        LaunchSpecification=launch_spec,
         ValidUntil=validUntil,
     )
 
@@ -55,16 +55,5 @@ if spot:
 
     print r
 else:
-    r = client.run_instances(
-        ImageId='ami-5189a661', # Ubuntu 14.04
-        MinCount=1,
-        MaxCount=1,
-        KeyName='Main Key Pair',
-        SecurityGroups=['indexer'],
-        UserData=userData,
-        InstanceType='c3.2xlarge',
-        BlockDeviceMappings=blockDevices,
-        IamInstanceProfile={'Name': 'indexer-role'},
-    )
-
+    r = client.run_instances(MinCount=1, MaxCount=1, **launch_spec)
     print r
