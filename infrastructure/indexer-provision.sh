a46c83290208e278fc671ea12732accc2b9373c8
Bill McCloskey <billm@mozilla.com>
2017-01-23 11:36:57 -0800

    Change link to SpiderMonkey shell

--- infrastructure/indexer-provision.sh
+++ infrastructure/indexer-provision.sh
@@ -81,10 +81,10 @@ echo Config repository is $CONFIG_REPO
 
 # Install SpiderMonkey.
 rm -rf jsshell-linux-x86_64.zip js
-wget -q https://index.taskcluster.net/v1/task/gecko.v2.mozilla-central.nightly.latest.firefox.linux64-opt/artifacts/public/build/jsshell-linux-x86_64.zip
+wget -q https://index.taskcluster.net/v1/task/gecko.v2.mozilla-central.nightly.latest.firefox.linux64-opt/artifacts/public/build/target.jsshell.zip
 mkdir js
 pushd js
-unzip ../jsshell-linux-x86_64.zip
+unzip ../target.jsshell.zip
 sudo install js /usr/local/bin
 sudo install *.so /usr/local/lib
 sudo ldconfig
