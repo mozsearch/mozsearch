577557b288249e811f9f57237fc9525eccede97e
Bill McCloskey <billm@mozilla.com>
2017-11-08 16:15:02 -0800

    Add missing dep for web server provisioning

--- infrastructure/web-server-provision.sh
+++ infrastructure/web-server-provision.sh
@@ -7,6 +7,7 @@ sudo apt-get update
 sudo apt-get install -y git
 
 # Livegrep (Bazel is needed for Livegrep builds)
+sudo apt-get install -y openjdk-8-jdk
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 curl https://storage.googleapis.com/bazel-apt/doc/apt-key.pub.gpg | sudo apt-key add -
 sudo apt-get update
