d50568e136cea44038e1d5968dcb83af6dc821a6
Kartikaya Gupta <kgupta@mozilla.com>
2018-02-12 11:49:22 -0500

    Update rust-analyze.sh to handle multiple save-analysis directories

--- scripts/rust-analyze.sh
+++ scripts/rust-analyze.sh
@@ -15,11 +15,10 @@ TREE_NAME=$2
 MOZSEARCH_PATH=$(cd $(dirname "$0") && git rev-parse --show-toplevel)
 . $MOZSEARCH_PATH/scripts/load-vars.sh $CONFIG_FILE $TREE_NAME
 
-ANALYSIS_DIR=$(find $OBJDIR -type d -name save-analysis || true)
-
-if [ -d "$ANALYSIS_DIR" ]; then
+find $OBJDIR -type d -name save-analysis |
+while read ANALYSIS_DIR; do
   $MOZSEARCH_PATH/tools/target/release/rust-indexer \
-    $FILES_ROOT \
-    $ANALYSIS_DIR \
-    $INDEX_ROOT/analysis
-fi
+    "$FILES_ROOT" \
+    "$ANALYSIS_DIR" \
+    "$INDEX_ROOT/analysis"
+done
