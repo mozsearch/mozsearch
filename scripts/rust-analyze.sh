55b3b237657b459234c1939ddbf6affa8c68c29f
Emilio Cobos Álvarez <emilio@crisal.io>
2018-03-27 13:45:25 +0200

    rust-indexer: Handle generated files properly.
    It's not clear the shell script accounts correctly for whitespace in the paths.
    
    That being said it's not clear other scripts do so, and we have control over the
    paths anyway.

--- scripts/rust-analyze.sh
+++ scripts/rust-analyze.sh
@@ -12,10 +12,11 @@ set -x # Show commands
 CONFIG_FILE=$(realpath $1)
 TREE_NAME=$2
 
-find $OBJDIR -type d -name save-analysis |
-while read ANALYSIS_DIR; do
+if [ -d "$OBJDIR" ]; then
+  ANALYSIS_DIRS="$(find $OBJDIR -type d -name save-analysis)"
   $MOZSEARCH_PATH/tools/target/release/rust-indexer \
     "$FILES_ROOT" \
-    "$ANALYSIS_DIR" \
-    "$INDEX_ROOT/analysis"
-done
+    "$INDEX_ROOT/analysis" \
+    "$OBJDIR" \
+    $ANALYSIS_DIRS
+fi
