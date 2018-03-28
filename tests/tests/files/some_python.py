57b64fd147d915f379802a653137dfb833457f87
Kartikaya Gupta <kgupta@mozilla.com>
2018-03-28 12:21:37 -0400

    Bug 1449606 - Linkify bug numbers and links in string literals

---
+++ tests/tests/files/some_python.py
@@ -0,0 +1,5 @@
+def myFunction():
+    """This is the function documentation.
+    As per https://bugzil.la/1449606 (i.e. bug 1449606) we want
+    URLs and bug numbers here to be linkified."""
+    pass
