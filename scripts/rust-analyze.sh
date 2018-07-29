c46fd301952c056f713e07753f3bc068e6d953b0
Emilio Cobos Álvarez <emilio@crisal.io>
2018-07-30 01:41:03 +0200

    Bug 1545968 - Index rust standard library source.
    This reuses the generated files infrastructure just for convenience.

--- scripts/rust-analyze.sh
+++ scripts/rust-analyze.sh
@@ -15,11 +15,17 @@ TREE_NAME=$2
 
 if [ -d "$OBJDIR" ]; then
   ANALYSIS_DIRS="$(find $OBJDIR -type d -name save-analysis)"
-  if [ "x$ANALYSIS_DIRS" != "x" ]; then
-    $MOZSEARCH_PATH/tools/target/release/rust-indexer \
-      "$FILES_ROOT" \
-      "$INDEX_ROOT/analysis" \
-      "$OBJDIR" \
-      $ANALYSIS_DIRS
+  if [ "x$ANALYSIS_DIRS" = "x" ]; then
+    exit 0 # Nothing to analyze really.
   fi
+  # If we have rust stdlib sources and analysis data, pick that up too
+  if [ -d "$INDEX_ROOT/rustlib" ]; then
+    ANALYSIS_DIRS="$ANALYSIS_DIRS $(find $INDEX_ROOT/rustlib -type d -name analysis)"
+  fi
+  $MOZSEARCH_PATH/tools/target/release/rust-indexer \
+    "$FILES_ROOT" \
+    "$INDEX_ROOT/analysis" \
+    "$OBJDIR" \
+    "$INDEX_ROOT/rustlib/src/rust/src" \
+    $ANALYSIS_DIRS
 fi
