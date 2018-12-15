24f0610f300cc9975422f62b2ca52638e8b91b3f
Kartikaya Gupta <kgupta@mozilla.com>
2018-12-15 08:53:56 -0500

    Add templates5.cpp

---
+++ tests/tests/files/templates5.cpp
@@ -0,0 +1,8 @@
+#include "templates5.h"
+
+bool func() {
+  nsTArray<int> intarray(3);
+  nsTArray<float> floatarray(3.5);
+  return intarray.Contains(5, 3) ||
+         floatarray.Contains(3.5, 5);
+}
