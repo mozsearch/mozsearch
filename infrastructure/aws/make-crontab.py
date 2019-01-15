10754b1ce98932ce08589c90a5ac3144a443b268
Kartikaya Gupta <kgupta@mozilla.com>
2019-01-15 12:02:39 -0500

    Bug 1544283 - Make the timeout email also go to the commit owner

--- infrastructure/aws/make-crontab.py
+++ infrastructure/aws/make-crontab.py
@@ -4,13 +4,16 @@ import datetime
 import os
 import subprocess
 
+subj_prefix = sys.argv[1]
+dest_email = sys.argv[2]
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 delta = datetime.timedelta(hours=6)
 when = datetime.datetime.now() + delta
 s = when.strftime('%M %H %d %m *')
 
-s += ' ' + os.path.join(dir_path, 'send-failure-email.py') + ' release-timeout searchfox-aws@mozilla.com\n'
+s += ' ' + os.path.join(dir_path, 'send-failure-email.py') + ' ' + subj_prefix + ' ' + dest_email + '\n'
 
 print s
 
