f5094ed3876055690c4f73bc8feb228a01bda8c4
Andrew Sutherland <asutherland@asutherland.org>
2019-09-02 18:46:20 -0400

    Bug 1567724 - Make indexing faster through use of scratch dir and upgrading to C5 instances. r=kats

--- infrastructure/aws/index.sh
+++ infrastructure/aws/index.sh
@@ -24,16 +24,36 @@ CONFIG_INPUT="$6"
 
 EC2_INSTANCE_ID=$(wget -q -O - http://instance-data/latest/meta-data/instance-id)
 
+
+echo "Creating index-scratch on local instance SSD"
+
+# Create the "index-scratch" directory where each specific tree's indexing
+# byproducts live while the indexing is ongoing.
+#
+# Under the old c2d image /dev/xvdb was the instance-locale storage and was
+# somehow already mounted as /mnt.  Under c5d it's /dev/nvme1n1 and wasn't
+# already mounted.  We just dynamically initialize it here.
+sudo mkfs -t ext4 /dev/nvme1n1
+sudo mount /dev/nvme1n1 /mnt
+sudo mkdir /mnt/index-scratch
+sudo chown ubuntu.ubuntu /mnt/index-scratch
+
+
 echo "Branch is $BRANCH"
 echo "Channel is $CHANNEL"
 
 export AWS_ROOT=$(realpath $MOZSEARCH_PATH/infrastructure/aws)
 VOLUME_ID=$(python $AWS_ROOT/attach-index-volume.py $CHANNEL $EC2_INSTANCE_ID)
 
+# The EBS volume will no longer be mounted at /dev/xvdf but instead at an
+# arbitrarily assigned nvme id.  However, since we only have a single EBS volume
+# and we dynamically attach it, we're pretty certain what the ID will be:
+EBS_NVME_DEV=nvme2n1
+
 set +o pipefail   # The grep command below can return nonzero, so temporarily allow pipefail
-while true
+for (( i = 0; i < 3600; i++ ))
 do
-    COUNT=$(lsblk | grep xvdf | wc -l)
+    COUNT=$(lsblk | grep $EBS_NVME_DEV | wc -l)
     if [ $COUNT -eq 1 ]
     then break
     fi
@@ -43,21 +63,26 @@ set -o pipefail
 
 echo "Index volume detected"
 
-sudo mkfs -t ext4 /dev/xvdf
+# Create the "index" directory where the byproducts of indexing will permanently
+# live.
+sudo mkfs -t ext4 /dev/$EBS_NVME_DEV
 sudo mkdir /index
-sudo mount /dev/xvdf /index
+sudo mount /dev/$EBS_NVME_DEV /index
 sudo chown ubuntu.ubuntu /index
 
-$MOZSEARCH_PATH/infrastructure/indexer-setup.sh $CONFIG_REPO_PATH $CONFIG_INPUT /index
+# Do indexer setup locally on disk.
+$MOZSEARCH_PATH/infrastructure/indexer-setup.sh $CONFIG_REPO_PATH $CONFIG_INPUT /mnt/index-scratch
 case "$CHANNEL" in
 release | mozilla-releases )
     # Only upload files on release channels.
-    $MOZSEARCH_PATH/infrastructure/indexer-upload.sh $CONFIG_REPO_PATH /index
+    $MOZSEARCH_PATH/infrastructure/indexer-upload.sh $CONFIG_REPO_PATH /mnt/index-scratch
     ;;
 * )
     ;;
 esac
-$MOZSEARCH_PATH/infrastructure/indexer-run.sh $CONFIG_REPO_PATH /index
+# Now actually run the indexing, telling the scripts to move the data to the
+# permanent index directory.
+$MOZSEARCH_PATH/infrastructure/indexer-run.sh $CONFIG_REPO_PATH /mnt/index-scratch /index
 
 date
 echo "Indexing complete"
