be54745302fdd0983621ed5073de4de0b59791e2
Kartikaya Gupta <kgupta@mozilla.com>
2020-03-03 11:20:46 -0500

    Bug 1579464 - Retry the describe_instances call a few times if it throws an exception

--- infrastructure/aws/awslib.py
+++ infrastructure/aws/awslib.py
@@ -14,13 +14,21 @@ def await_volume(client, volumeId, waitingState, finishedState):
         sys.exit(1)
 
 def await_instance(client, instanceId, waitingState, finishedState):
+    exceptionCount = 0
     while True:
-        instances = client.describe_instances(InstanceIds=[instanceId])
-        state = instances['Reservations'][0]['Instances'][0]['State']['Name']
-        if waitingState and state != waitingState:
-            break
-        if state == finishedState:
-            break
+        try:
+            instances = client.describe_instances(InstanceIds=[instanceId])
+            state = instances['Reservations'][0]['Instances'][0]['State']['Name']
+            if waitingState and state != waitingState:
+                break
+            if state == finishedState:
+                break
+        except:
+            if exceptionCount > 2:
+                raise
+            exceptionCount += 1
+            print('Unable to describe instance ID {}, will retry...'.format(instanceId))
+            time.sleep(10)
         time.sleep(1)
 
     if state != finishedState:
