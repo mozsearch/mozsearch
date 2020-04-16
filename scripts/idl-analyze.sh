d70cd4403eeaba63d339cc5de94dd5ce8dac3562
Kartikaya Gupta <kgupta@mozilla.com>
2020-04-16 08:33:11 -0400

    Bug 1630593 - Use the in-tree xpidl parser if there is one.

--- scripts/idl-analyze.sh
+++ scripts/idl-analyze.sh
@@ -13,21 +13,37 @@ fi
 CONFIG_FILE=$(realpath $1)
 TREE_NAME=$2
 
-if [ ! -d /tmp/pymodules ]
-then
-    mkdir /tmp/pymodules
-    pushd /tmp/pymodules
-    wget "https://hg.mozilla.org/mozilla-central/raw-file/tip/xpcom/idl-parser/xpidl/xpidl.py"
+if [ -f "${FILES_ROOT}/xpcom/idl-parser/xpidl/xpidl.py" ]; then
+    # The tree we're processing has an xpidl.py, so let's use that
+    TREE_PYMODULES="/tmp/pymodules-${TREE_NAME}"
+    PULL_FROM_MC=0
+else
+    # The tree we're processing doesn't have an xpidl.py, so we'll pull m-c's copy with wget
+    TREE_PYMODULES="/tmp/pymodules"
+    PULL_FROM_MC=1
+fi
+if [ ! -d "${TREE_PYMODULES}" ]; then
+    mkdir "${TREE_PYMODULES}"
+    pushd "${TREE_PYMODULES}"
+    if [ $PULL_FROM_MC -eq 0 ]; then
+        cp "${FILES_ROOT}/xpcom/idl-parser/xpidl/xpidl.py" ./
+    else
+        wget "https://hg.mozilla.org/mozilla-central/raw-file/tip/xpcom/idl-parser/xpidl/xpidl.py"
+    fi
     mkdir ply
     pushd ply
-    wget "https://hg.mozilla.org/mozilla-central/raw-file/tip/other-licenses/ply/ply/__init__.py"
-    wget "https://hg.mozilla.org/mozilla-central/raw-file/tip/other-licenses/ply/ply/lex.py"
-    wget "https://hg.mozilla.org/mozilla-central/raw-file/tip/other-licenses/ply/ply/yacc.py"
+    for PLYFILE in __init__.py lex.py yacc.py; do
+        if [ $PULL_FROM_MC -eq 0 ]; then
+            cp "${FILES_ROOT}/other-licenses/ply/ply/${PLYFILE}" ./
+        else
+            wget "https://hg.mozilla.org/mozilla-central/raw-file/tip/other-licenses/ply/ply/${PLYFILE}"
+        fi
+    done
     popd
     popd
 fi
 
-export PYTHONPATH=/tmp/pymodules
+export PYTHONPATH="${TREE_PYMODULES}"
 
 cat $INDEX_ROOT/idl-files | \
     parallel python $MOZSEARCH_PATH/scripts/idl-analyze.py \
