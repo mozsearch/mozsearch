2c8e1e75aa0e48198b5ef670294ca48b47d000b5
Kartikaya Gupta <kgupta@mozilla.com>
2020-04-18 04:14:35 -0400

    Bug 1625758 - Prompt to restart instance if it's not running

--- infrastructure/aws/ssh.py
+++ infrastructure/aws/ssh.py
@@ -12,7 +12,10 @@ import sys
 import subprocess
 import time
 
+import awslib
+
 ec2 = boto3.resource('ec2')
+client = boto3.client('ec2')
 
 def print_instances():
     now = None
@@ -20,6 +23,8 @@ def print_instances():
     for instance in ec2.instances.all():
         if len(instance.security_groups) != 1:
             continue
+
+        state = instance.state['Name']
 
         group = instance.security_groups[0]['GroupName']
 
@@ -38,7 +43,37 @@ def print_instances():
         # strip off sub-seconds
         age_str = age_str[:age_str.find('.')]
 
-        print(instance.id, group, age_str, ["%s: %s" % (k, tags[k]) for k in sorted(tags.keys())])
+        print(instance.id, state, group, age_str, ["%s: %s" % (k, tags[k]) for k in sorted(tags.keys())])
+
+def prompt(text):
+    while True:
+        reply = str(raw_input(text + " (y/n) ")).lower()
+        if reply[0] == 'y':
+            return True
+        elif reply[0] == 'n':
+            return False
+
+def ensure_started(instance):
+    state = instance.state['Name']
+    if state == 'running':
+        return False
+
+    if not prompt("Instance is currently %s, attempt to start it?" % state):
+        print("Cannot connect to stopped instance!")
+        sys.exit(1)
+
+    client.start_instances(InstanceIds=[instance.id])
+    print("Awaiting instance start...")
+    awslib.await_instance(client, instance.id, None, 'running')
+    return state
+
+def restore_state(instance, old_state):
+    if old_state == 'stopped':
+        client.stop_instances(InstanceIds=[instance.id])
+        print("Awaiting instance stop...")
+        awslib.await_instance(client, instance.id, None, old_state)
+    else:
+        print("Unrecognized initial state %s, cannot restore state!", old_state)
 
 def change_security(instance, make_secure):
     secure_suffix = '-secure'
@@ -61,6 +96,7 @@ def change_security(instance, make_secure):
     instance.modify_attribute(Groups=[new_group.id])
 
 def log_into(instance):
+    old_state = ensure_started(instance)
     change_security(instance, False)
 
     # If there is a private key at ~/.aws/private_key.pem, use it
@@ -75,6 +111,9 @@ def log_into(instance):
     p.wait()
 
     change_security(instance, True)
+    if old_state is not False:
+        if prompt("Instance was started before connection, attempt to restore original state '%s'?" % old_state):
+            restore_state(instance, old_state)
 
     sys.exit(p.returncode)
 
