da19140dfb89119bdcfb765ece93bca3328cab80
Emilio Cobos Álvarez <emilio@crisal.io>
2020-04-23 03:17:36 +0200

    No bug - Rewrite the code highlighter.
    This is another piece of the code which was doing something relatively
    simple, in an extremely complex way.
    
    The only thing this needs to do is keeping a set of selected lines and
    represent them in the DOM, knowing how to highlight them, serializing
    them from an URL hash, and parsing that URL hash.
    
    I couldn't understand any of the previous code. We don't use the
    "clicked" and "multihightlight" classes, so unify that code.
    
    This still uses createSyntheticAnchor. I'll get to that.

--- static/js/code-highlighter.js
+++ static/js/code-highlighter.js
@@ -169,364 +169,187 @@ var Sticky = new (class Sticky {
   }
 })();
 
-$(function () {
-  "use strict";
-  var container = $("#file");
-  var lastModifierKey = null; // use this as a sort of canary/state indicator showing the last user action
-  var singleLinesArray = []; //track single highlighted lines here
-  var rangesArray = []; // track ranges of highlighted lines here
-
-  //sort a one dimensional array in Ascending order
-  function sortAsc(a, b) {
-    return a - b;
-  }
-  function stringToRange(a) {
-    a = a.split("-");
-    a[0] = parseInt(a[0], 10);
-    a[1] = parseInt(a[1], 10);
-    return a;
-  }
-  function sortRangeAsc(a, b) {
-    // tweak in order to account for inverted ranges like 150-120
-    return Math.min(a[0], a[1]) - Math.min(b[0], b[1]);
-  }
-  function lineFromId(id) {
-    if (id) {
-      return id.slice(1);
+var Highlight = new (class Highlight {
+  constructor() {
+    for (let line of document.querySelectorAll(".line-number")) {
+      line.addEventListener("click", event => this.onLineNumberClick(event));
     }
-    return id;
-  }
-  /**
-   * Scours the current DOM and returns [array of all single-selected line
-   * Numbers, array of all multi-selected line Numbers] suitable for
-   * de-duplication and range-derivation.
-   */
-  function generateSelectedArrays() {
-    var line = null;
-    var rangeMax = null;
-    var lines = [];
-    var rangesArray = [];
-    var singleLinesArray = [];
-
-    var multiSelected = $(".line-number.multihighlight");
-    var singleSelected = $(".line-number.highlighted");
-
-    function generateLines(selected, lines) {
-      for (var i = 0; i < selected.length; i++) {
-        if (selected[i].id) {
-          lines.push(parseInt(lineFromId(selected[i].id), 10));
-        }
-      }
-      return lines;
-    }
-
-    lines = generateLines(multiSelected, lines);
-    lines = generateLines(singleSelected, lines);
-
-    // strip all single lines, e.g. those without an adjacent line+1 == nextLine
-    for (var s = lines.length - 1; s >= 0; s--) {
-      line = lines[s];
-      // this presumes selected is sorted in asc order, if not it won't work
-      if (line !== lines[s + 1] - 1 && line !== lines[s - 1] + 1) {
-        singleLinesArray.push(line);
-        lines.splice(s, 1);
-      }
-    }
-
-    //this presumes selected is sorted in asc order after single lines have been removed
-    while (lines.length > 0) {
-      line = lines[0];
-      var pos = 1;
-      while (line === lines[pos] - pos) {
-        rangeMax = lines[pos];
-        pos++;
-      }
-      rangesArray.push([line, rangeMax]);
-      lines.splice(0, pos);
-    }
-    //return sorted arrays
-    return [singleLinesArray.sort(sortAsc), rangesArray.sort(sortRangeAsc)];
+    this.lastSelectedLine = null;
+    this.selectedLines = new Set();
+    this.updateFromHash();
   }
 
-  /**
-   * Reflects the current state of selected lines per the DOM into the location
-   * hash, updating the current history entry.
-   */
-  function setWindowHash() {
-    var windowHash = null;
-    var s = null;
-    var r = null;
-    var reCleanup = /(^#?,|,$)/;
-    var selectedArray = generateSelectedArrays(); // generates sorted arrays
-    var singleLinesArray = selectedArray[0];
-    var rangesArray = selectedArray[1];
-    // eliminate duplication
-    for (s = 0; s < singleLinesArray.length; s++) {
-      for (r = 0; r < rangesArray.length; r++) {
-        if (s >= rangesArray[r][0] && s <= rangesArray[r][1]) {
-          singleLinesArray.splice(s, 1);
-          s--;
-        }
-      }
-    }
-    if (singleLinesArray.length || rangesArray.length) {
-      windowHash = "#";
-    }
-    for (
-      s = 0, r = 0;
-      s < singleLinesArray.length || r < rangesArray.length;
-
-    ) {
-      // if no ranges left or singleLine < range add singleLine to hash
-      // if no singleLines left or range < singleLine add range to hash
-      if (r == rangesArray.length || singleLinesArray[s] < rangesArray[r][0]) {
-        windowHash += singleLinesArray[s] + ",";
-        s++;
-      } else if (
-        s == singleLinesArray.length ||
-        rangesArray[r][0] < singleLinesArray[s]
-      ) {
-        windowHash += rangesArray[r][0] + "-" + rangesArray[r][1] + ",";
-        r++;
-      }
-    }
-    if (windowHash) {
-      windowHash = windowHash.replace(reCleanup, "");
-      history.replaceState(null, "", windowHash);
-      let lineno = windowHash.slice(1);
-      createSyntheticAnchor(lineno, false);
-
-      $("a[data-update-link]").each(function (i, elt) {
-        let extra = $(elt).attr("data-update-link").replace("{}", lineno);
-        $(elt).attr("href", $(elt).attr("data-link") + extra);
-      });
-    }
+  addSelectedLine(line) {
+    document.getElementById("l" + line).classList.add("highlighted");
+    document.getElementById("line-" + line).classList.add("highlighted");
+    // NOTE: The order here is intentional so that we throw above if the line
+    // is not in the document.
+    this.selectedLines.add(line);
+    this.lastSelectedLine = line;
   }
 
-  //parse window.location.hash on new requsts into two arrays
-  //one of single lines and one multilines
-  //use with singleLinesArray and rangesArray for adding/changing new highlights
-  function getSortedHashLines() {
-    var highlights = window.location.hash.substring(1);
-    var lineStart = null;
-    var reRanges = /[0-9]+-[0-9]+/g;
-    var reCleanup = /[^0-9,]/g;
-    var ranges = null;
-    var firstRange = null;
-    highlights = highlights.replace(/ /g, ""); // clean whitespace
-    ranges = highlights.match(reRanges);
-    if (ranges !== null) {
-      ranges = ranges.map(stringToRange).sort(sortRangeAsc);
-      //strip out multiline items like 12-15, so that all that is left are single lines
-      //populate rangesArray for reuse if a user selects more ranges later
-      for (var i = 0; i < ranges.length; i++) {
-        highlights = highlights.replace(ranges[i].join("-"), "");
-        ranges[i].sort(sortAsc);
-      }
-      // add the ordered ranges to the rangesArray
-      rangesArray = rangesArray.concat(ranges);
-      firstRange = rangesArray[0];
-      highlights = highlights.replace(reCleanup, ""); // clean anything other than digits and commas
-      highlights = highlights.replace(/,,+/g, ","); // clean multiple commas
-      highlights = highlights.replace(/^,|,$/g, ""); // clean leading and tailing comas
+  removeSelectedLine(line) {
+    this.selectedLines.delete(line);
+    if (this.lastSelectedLine == line) {
+      this.lastSelectedLine = null;
     }
+    document.getElementById("l" + line).classList.remove("highlighted");
+    document.getElementById("line-" + line).classList.remove("highlighted");
+  }
 
-    if (highlights.length) {
-      //make an array of integers and sort it for the remaining single lines
-      highlights = highlights.split(",");
-      for (var h = 0; h < highlights.length; h++) {
-        highlights[h] = parseInt(highlights[h], 10);
-        //in case some unwanted string snuck by remove it
-        if (isNaN(highlights[h])) {
-          highlights.splice(h, 1);
-          h--;
-        }
-      }
-      highlights = highlights.sort(sortAsc);
-      //set the global singleLinesArry for reuse
-      singleLinesArray = highlights;
+  toggleSelectedLine(line) {
+    if (this.selectedLines.has(line)) {
+      this.removeSelectedLine(line);
     } else {
-      //this happens if there is no single line in a url
-      //without setting this the url gets an NaN element in it
-      highlights = null;
+      this.addSelectedLine(line);
     }
-
-    //a url can look like foo#12,15,20-25 or foo#12-15,18,20-25 or foo#1,2,3 etc.
-    //the lineStart should be the smallest integer in the single or highlighted ranges
-    //this ensures a proper position to which to scroll once the page loads
-    if (firstRange !== null && highlights !== null) {
-      if (highlights[0] < firstRange[0]) {
-        lineStart = highlights[0];
-      } else if (highlights[0] > firstRange[0]) {
-        lineStart = firstRange[0];
-      }
-    } else if (firstRange !== null && highlights === null) {
-      lineStart = firstRange[0];
-    } else if (firstRange === null && highlights !== null) {
-      lineStart = highlights[0];
-    } else {
-      lineStart = null;
-    }
-
-    return {
-      lineStart: lineStart,
-      highlights: singleLinesArray,
-      ranges: rangesArray,
-    };
   }
 
-  //first bind to all .line-number click events only
-  container.on("click", ".line-number", function (event) {
-    // hacky logic to jump if this is a stuck line number
+  removeAllLines() {
+    for (let line of [...this.selectedLines]) {
+      this.removeSelectedLine(line);
+    }
+  }
+
+  selectSingleLine(line) {
+    this.removeAllLines();
+    this.addSelectedLine(line);
+  }
+
+  onLineNumberClick(event) {
     if (!event.shiftKey && !event.ctrlKey) {
-      var containingLine = $(this).closest(".source-line-with-number")[0];
-      if (containingLine) {
-        if (containingLine.classList.contains("stuck")) {
-          document.getElementById("scrolling").scrollTop -=
-            containingLine.offsetTop;
-          return;
-        }
+      // Hacky logic to jump if this is a stuck line number
+      //
+      // TODO(emilio): This should probably select the line as well, or something?
+      let containingLine = event.target.closest(".source-line-with-number");
+      if (containingLine && containingLine.classList.contains("stuck")) {
+        Sticky.scroller.scrollTop -= containingLine.offsetTop;
+        return;
       }
     }
 
-    var clickedNum = parseInt(lineFromId($(this).attr("id")), 10); // get the clicked line number
-    var line = $("#l" + clickedNum + ", #line-" + clickedNum); // get line-number and code
-    var lastSelectedLine = $(
-      ".line-number.last-selected, .source-line.last-selected"
-    );
-    var lastSelectedNum = parseInt(
-      lineFromId($(".line-number.last-selected").attr("id")),
-      10
-    ); // get last selected line number as integer
-    var selectedLineNums = null; // used for selecting elements with class .line-number
-    var selectedLineCode = null; // used for selecting code elements in .code class
-    var self = this;
-
-    function pickLineNums(lowInclusive, highExclusive) {
-      let count = highExclusive - lowInclusive;
-      let ids = new Array(count);
-      for (let i = 0; i < count; i++) {
-        ids[i] = `#l${lowInclusive + i}`;
-      }
-      return $(ids.join(", "));
-    }
-    function pickLineCodes(lowInclusive, highExclusive) {
-      let count = highExclusive - lowInclusive;
-      let ids = new Array(count);
-      for (let i = 0; i < count; i++) {
-        ids[i] = `#line-${lowInclusive + i}`;
-      }
-      return $(ids.join(", "));
-    }
-
-    //multiselect on shiftkey modifier combined with click
+    let line = parseInt(event.target.id.substring(1), 10);
     if (event.shiftKey) {
-      var classToAdd = "multihighlight";
-      // on shift, find last-selected code element
-      // if lastSelectedNum less than clickedNum go back
-      // else if lastSelectedNum greater than line id, go forward
-      if (lastSelectedNum === clickedNum) {
-        //toggle a single shiftclicked line
-        line.removeClass("last-selected highlighted clicked multihighlight");
-      } else if (lastSelectedNum < clickedNum) {
-        //shiftclick descending down the page
-        line.addClass("clicked");
-        selectedLineNums = pickLineNums(lastSelectedNum, clickedNum);
-        selectedLineCode = pickLineCodes(lastSelectedNum, clickedNum);
-        $(".last-selected").removeClass("clicked");
-      } else if (lastSelectedNum > clickedNum) {
-        //shiftclick ascending up the page
-        $(".line-number, .source-line").removeClass("clicked");
-        line.addClass("clicked");
-        selectedLineNums = pickLineNums(clickedNum, lastSelectedNum);
-        selectedLineCode = pickLineCodes(clickedNum, lastSelectedNum);
-      }
-      selectedLineNums.addClass(classToAdd);
-      selectedLineCode.addClass(classToAdd);
-
-      //set the last used modifier key
-      lastModifierKey = "shift";
-      // since all highlighed items are stripped, add one back, mark new last-selected
-      lastSelectedLine
-        .addClass(classToAdd)
-        .removeClass("last-selected highlighted");
-      //line.removeClass('highlighted');
-      line.addClass(classToAdd);
-      line.addClass("last-selected");
-    } else if (event.shiftKey && lastModifierKey === "singleSelectKey") {
-      //if ctrl/command was last pressed, add multihighlight class to new lines
-      $(".line-number, .source-line").removeClass("clicked");
-      line.addClass("clicked");
-      selectedLineNums = pickLineNums(lastSelectedNum, clickedNum);
-      selectedLineNums.addClass("multihighlight").removeClass("highlighted");
-      selectedLineCode = pickLineCodes(lastSelectedNum, clickedNum);
-      selectedLineCode.addClass("multihighlight").removeClass("highlighted");
-      line.addClass("multihighlight");
-    } else if (event.ctrlKey || event.metaKey) {
-      //a single click with ctrl/command highlights one line and preserves existing highlights
-      lastModifierKey = "singleSelectKey";
-      $(".highlighted").addClass("multihighlight");
-      $(".line-number, .source-line").removeClass(
-        "last-selected clicked highlighted"
-      );
-      if (lastSelectedNum !== clickedNum) {
-        line.toggleClass("clicked last-selected multihighlight");
+      // Range-select on shiftkey.
+      //
+      // TODO(emilio): This should maybe toggle instead of just adding to the
+      // selection?
+      if (!this.lastSelectedLine) {
+        this.addSelectedLine(line);
+      } else if (this.lastSelectedLine == line) {
+        this.removeSelectedLine(line);
+      } else if (this.lastSelectedLine < line) {
+        for (let i = this.lastSelectedLine; i < line; ++i) {
+          this.addSelectedLine(i + 1);
+        }
       } else {
-        line.toggleClass("multihighlight");
-        history.replaceState(null, "", "#");
-      }
-    } else {
-      //set lastModifierKey ranges and single lines to null, then clear all highlights
-      lastModifierKey = null;
-      //Remove existing highlights.
-      $(".line-number, .source-line").removeClass(
-        "last-selected highlighted multihighlight clicked"
-      );
-      //empty out single lines and ranges arrays
-      rangesArray = [];
-      singleLinesArray = [];
-      //toggle highlighting on for any line that was not previously clicked
-      if (lastSelectedNum !== clickedNum) {
-        //With this we're one better than github, which doesn't allow toggling single lines
-        line.toggleClass("last-selected highlighted");
-      } else {
-        history.replaceState(null, "", "#");
-      }
-    }
-    setWindowHash();
-  });
-
-  //highlight line(s) if someone visits a url directly with an #anchor
-  $(document).ready(function () {
-    if (window.location.hash.substring(1)) {
-      var toHighlight = getSortedHashLines(),
-        highlights = toHighlight.highlights,
-        ranges = toHighlight.ranges;
-
-      if (highlights !== null) {
-        //add single line highlights
-        for (var i = 0; i < highlights.length; i++) {
-          $("#l" + highlights[i] + ", #line-" + highlights[i]).addClass(
-            "highlighted"
-          );
+        for (let i = this.lastSelectedLine; i > line; --i) {
+          this.addSelectedLine(i - 1);
         }
       }
+    } else if (event.ctrlKey || event.metaKey) {
+      // Toggle select on ctrl / meta.
+      this.toggleSelectedLine(line);
+    } else {
+      this.selectSingleLine(line);
+    }
+    this.updateHash();
+  }
 
-      if (ranges !== null) {
-        //handle multiple sets of multi-line highlights from an incoming url
-        for (var j = 0; j < ranges.length; j++) {
-          //handle a single set of line ranges here; the c counter must be <= since it is a line id
-          for (var c = ranges[j][0]; c <= ranges[j][1]; c++) {
-            $("#l" + c + ", #line-" + c).addClass("highlighted");
+  updateHash() {
+    let hash = this.toHash();
+    {
+      let historyHash = hash ? "#" + hash : "";
+      if (historyHash != window.location.hash) {
+        window.history.replaceState(null, "", historyHash);
+      }
+    }
+    if (hash) {
+      createSyntheticAnchor(hash, false);
+    }
+    for (let link of document.querySelectorAll("a[data-update-link]")) {
+      let extra = link.getAttribute("data-update-link").replace("{}", hash);
+      link.href = link.getAttribute("data-link") + extra;
+    }
+  }
+
+  toHash() {
+    if (!this.selectedLines.size) {
+      return "";
+    }
+    // Try to create ranges out of the lines.
+    let lines = [...this.selectedLines].sort((a, b) => a - b);
+    let ranges = [];
+    let current = { start: lines[0], end: lines[0] };
+    for (let i = 1; i < lines.length; ++i) {
+      if (lines[i] == current.end + 1) {
+        current.end += 1;
+      } else {
+        ranges.push(current);
+        current = { start: lines[i], end: lines[i] };
+      }
+    }
+    ranges.push(current);
+    return ranges
+      .map(range => {
+        if (range.start == range.end) {
+          return range.start + "";
+        }
+        return range.start + "-" + range.end;
+      })
+      .join(",");
+  }
+
+  updateFromHash() {
+    this.removeAllLines();
+    let hash = window.location.hash.substring(1);
+    if (!hash) {
+      return;
+    }
+    for (let chunk of hash.split(",")) {
+      if (!chunk.length) {
+        continue;
+      }
+      let range = chunk.split("-");
+      if (range.length == 1) {
+        let line = parseInt(range[0], 10);
+        if (isNaN(line)) {
+          continue;
+        }
+        try {
+          this.addSelectedLine(line);
+        } catch (ex) {
+          // The line may not be in the document.
+        }
+      } else if (range.length == 2) {
+        let first = parseInt(range[0], 10);
+        let second = parseInt(range[1], 10);
+        if (isNaN(first) || isNaN(second)) {
+          continue;
+        }
+        // Allow inverted ranges in the url, in case they're manually written
+        // or what not.
+        let start = Math.min(first, second);
+        let end = Math.max(first, second);
+        for (let i = start; i <= end; ++i) {
+          try {
+            this.addSelectedLine(i);
+          } catch (ex) {
+            // The line may not be in the document.
           }
         }
+      } else {
+        // Something unknown, ignore.
       }
-
-      // tidy up an incoming url that might be typed in manually
-      setWindowHash();
     }
-  });
-});
+    // We're done parsing the hash, now update so we use the sanitized version
+    // if we have at least one line selected. Otherwise it could be an idref or
+    // something of that sort.
+    if (this.selectedLines.size) {
+      this.updateHash();
+    }
+  }
+})();
 
 // We use user-select:none to hide line numbers from text selections. That
 // does however not work in Chrome (https://crbug.com/850685)
