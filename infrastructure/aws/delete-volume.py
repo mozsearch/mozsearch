ebf82d13f383e23a77859932711495676d7575ce
Kartikaya Gupta <kgupta@mozilla.com>
2020-05-15 14:08:57 -0400

    Bug 1616362 - Remove double parentheses on prints.

--- infrastructure/aws/delete-volume.py
+++ infrastructure/aws/delete-volume.py
@@ -17,8 +17,8 @@ ec2 = boto3.client('ec2')
 volumeId = sys.argv[1]
 volume = ec2.describe_volumes(VolumeIds=[volumeId])['Volumes'][0]
 if volume['State'] != 'available':
-    print(("Volume is in state %s, waiting for it to go into state available..." % volume['State']))
+    print("Volume is in state %s, waiting for it to go into state available..." % volume['State'])
     awslib.await_volume(ec2, volumeId, volume['State'], 'available')
 
 ec2.delete_volume(VolumeId=volumeId)
-print(("Volume %s deleted" % volumeId))
+print("Volume %s deleted" % volumeId)
