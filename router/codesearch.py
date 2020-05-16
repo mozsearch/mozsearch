7008d3b93926416d72bb617a4d47641f7430c965
Kartikaya Gupta <kgupta@mozilla.com>
2020-05-16 14:02:37 -0400

    Bug 1616362 - Avoid unbuffered text I/O, disallowed in py3.
    The open call either needs to be binary mode, or buffered.
    Since we're redirecting to dev/null anyway it doesn't matter if
    it's buffered or not, so let's just make it buffered. Failure
    to do this throws:
      ValueError: can't have unbuffered text I/O

--- router/codesearch.py
+++ router/codesearch.py
@@ -59,7 +59,7 @@ def daemonize(args):
 
     si = open('/dev/null', 'r')
     so = open('/dev/null', 'a+')
-    se = open('/dev/null', 'a+', 0)
+    se = open('/dev/null', 'a+')
     os.dup2(si.fileno(), sys.stdin.fileno())
     os.dup2(so.fileno(), sys.stdout.fileno())
     os.dup2(se.fileno(), sys.stderr.fileno())
