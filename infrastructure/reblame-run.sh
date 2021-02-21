a72a01badd2da370d3a2feebe5c201567fc240e6
Kartikaya Gupta <kats@trevize.staktrace.com>
2021-02-21 15:32:52 -0500

    Bug 1683563 - Add blame rebuilding scripts
    The trigger_blame_rebuild script is almost identical to trigger_indexer.py,
    and is intended to be run manually by devs who want to trigger a blame
    rebuild. As with indexer runs, it can run using code from a specific branch/fork
    and on a specific channel to test changes before actually deploying them.
    
    The entry point on the spawned EC2 instance is main.sh (same as with a regular
    indexer) but it then delegates to rebuild-blame.sh instead of index.sh. That
    handles some of the AWS setup/teardown steps while delegating the actual
    blame rebuilding steps to infrastructure/reblame-run.sh.
    
    This also adds a Makefile target that shows how to exercise the reblame-run.sh
    script in local vagrant environment on the NSS repo.
    
    Unlike the indexer, running trigger_blame_rebuild won't spawn a web server
    instance. Instead, it just uploads the new blame tarballs to S3 (with the -new
    suffix) and terminates itself.

---
+++ infrastructure/reblame-run.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -x # Show commands
+set -eu # Errors/undefined vars are fatal
+set -o pipefail # Check all commands in a pipeline
+
+if [ $# -lt 3 ]
+then
+    echo "usage: $0 <config-repo-path> <config-file-name> <working-dir> [extra-args-for-reblame]"
+    exit 1
+fi
+
+export MOZSEARCH_PATH=$(readlink -f $(dirname "$0")/..)
+export CONFIG_REPO=$(readlink -f $1)
+CONFIG_INPUT="$2"
+export WORKING=$(readlink -f $3)
+
+# Remove first three command-line args from the $* variable, so we're just left with the
+# "extra arguments" to pass on to the per-repo reblame script.
+shift 3
+
+if [ -z "${CLEAN_WORKING:-}" ]; then
+    echo "Keeping old contents of $WORKING/. Set CLEAN_WORKING=1 to remove the contents of $WORKING/."
+else
+    echo "Removing old contents of $WORKING/."
+    rm -rf $WORKING/*
+fi
+
+$MOZSEARCH_PATH/scripts/generate-config.sh $CONFIG_REPO $CONFIG_INPUT $WORKING
+CONFIG_FILE=$WORKING/config.json
+
+for TREE_NAME in $(jq -r ".trees|keys_unsorted|.[]" ${CONFIG_FILE})
+do
+    . $MOZSEARCH_PATH/scripts/load-vars.sh $CONFIG_FILE $TREE_NAME
+    mkdir -p $INDEX_ROOT
+    cd $INDEX_ROOT
+
+    if [ -f "$CONFIG_REPO/$TREE_NAME/reblame" ]; then
+        $CONFIG_REPO/$TREE_NAME/reblame $*
+    fi
+done
