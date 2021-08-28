516f8daa0cdbc493c443f458bd178bb2bc3fb12a
Andrew Sutherland <asutherland@asutherland.org>
2021-08-28 19:38:15 -0400

    Bug 1716167 - Add a way to launch an indexer shell
    This patch adds a new `trigger_shell.py` command and extracts the duplicated
    logic that existed between `trigger_indexer.py` and `trigger_blame_rebuild.py`
    up into `trigger_common.py` which now uses argparse as an argument parser.
    
    The primary motivation of the cleanup because optional arguments like `setenv`
    seem useful across all indexer types.  This would also be the case for things
    like allowing a command-line to override.
    
    As hinted above, we also add arguments for:
    - `--setenv` intended to be used like
      `--setenv TRYPUSH_REV=<40-char-rev-hash>`.
      - This specific try use-case is potentially a candidate for a custom arugment
        type perhaps with extra smarts.

--- infrastructure/aws/trigger_blame_rebuild.py
+++ infrastructure/aws/trigger_blame_rebuild.py
@@ -1,78 +1,23 @@
 #!/usr/bin/env python3
 
-import boto3
-from datetime import datetime, timedelta
-import sys
-import os.path
+from trigger_common import TriggerCommandBase
 
 # Usage: trigger_blame_rebuild.py <mozsearch-repo> <config-repo> <config-input> <branch> <channel>
 #  e.g.: trigger_blame_rebuild.py https://github.com/mozsearch/mozsearch https://github.com/mozsearch/mozsearch-mozilla config1.json master release
 
-def trigger(mozsearch_repo, config_repo, config_input, branch, channel):
-    ec2 = boto3.resource('ec2')
-    client = boto3.client('ec2')
+class TriggerReblameCommand(TriggerCommandBase):
+    def __init__(self):
+        timeout_hours = 7 * 24 # upper bound on how long we expect the blame-rebuild to take
+        super().__init__('blame-builder', 'rebuild-blame.sh', timeout_hours)
 
-    running = ec2.instances.filter(Filters=[{'Name': 'tag-key', 'Values': ['blame-builder']},
-                                           {'Name': 'tag:channel', 'Values': [channel]},
-                                           {'Name': 'instance-state-name', 'Values': ['running']}])
-    for instance in running:
-        print("Terminating existing running blame-rebuilder %s for channel %s" % (instance.instance_id, channel))
-        instance.terminate()
-
-    timeout_hours = 7 * 24 # upper bound on how long we expect the blame-rebuild to take
-    user_data = '''#!/usr/bin/env bash
-
-cd ~ubuntu
-sudo -i -u ubuntu ./update.sh "{branch}" "{mozsearch_repo}" "{config_repo}"
-sudo -i -u ubuntu mozsearch/infrastructure/aws/main.sh rebuild-blame.sh "{timeout_hours}" "{branch}" "{channel}" config "{config_input}"
-'''.format(branch=branch, channel=channel, mozsearch_repo=mozsearch_repo, config_repo=config_repo, config_input=config_input, timeout_hours=timeout_hours)
-
-    block_devices = []
-
-    images = client.describe_images(Filters=[{'Name': 'name', 'Values': ['indexer-20.04']}])
-    image_id = images['Images'][0]['ImageId']
-
-    launch_spec = {
-        'ImageId': image_id,
-        'KeyName': 'Main Key Pair',
-        'SecurityGroups': ['indexer-secure'],
-        'UserData': user_data,
-        'InstanceType': 'm5d.2xlarge',
-        'BlockDeviceMappings': block_devices,
-        'IamInstanceProfile': {
-            'Name': 'indexer-role',
-        },
-        'TagSpecifications': [{
-            'ResourceType': 'instance',
-            'Tags': [{
-                'Key': 'blame-builder',
-                'Value': str(datetime.now())
-            }, {
-                'Key': 'channel',
-                'Value': channel,
-            }, {
-                'Key': 'branch',
-                'Value': branch,
-            }, {
-                'Key': 'mrepo',
-                'Value': mozsearch_repo,
-            }, {
-                'Key': 'crepo',
-                'Value': config_repo,
-            }, {
-                'Key': 'cfile',
-                'Value': config_input,
-            }],
-        }],
-    }
-    return client.run_instances(MinCount=1, MaxCount=1, **launch_spec)
-
+    def script_args_after_branch_and_channel(self, args):
+        return '''config "{config_input}"'''.format(
+            mozsearch_repo=args.mozsearch_repo,
+            config_repo=args.config_repo,
+            config_input=args.config_input
+        )
 
 if __name__ == '__main__':
-    mozsearch_repo = sys.argv[1]
-    config_repo = sys.argv[2]
-    config_input = sys.argv[3]
-    branch = sys.argv[4]
-    channel = sys.argv[5]
-
-    trigger(mozsearch_repo, config_repo, config_input, branch, channel)
+    cmd = TriggerReblameCommand()
+    cmd.parse_args()
+    cmd.trigger()
