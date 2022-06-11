56e0346c0210fb98506eb0fcc259d8d7bc364d41
Andrew Sutherland <asutherland@asutherland.org>
2022-06-10 20:26:11 -0400

    Bug 1773767 - Update the IPDL parser and add limited test coverage.
    Because there's a lot of moving parts for the mozilla-central IPC
    implementation, we just copy analysis files from a web-server that
    has the C++ analysis data we want for the source files we want to show.
    
    This provides ipdl-analyze.rs with the analysis files it needs to
    scrape to establish the C++ symbol mapping.  And this is then enough
    for the test repository to give us a full fidelity experience of
    viewing the .ipdl file and sufficient fidelity when clicking on those
    associated Send/Recv methods as they relate to the IPDL file.  That's
    the end of our fidelity, although if we pulled down other source files
    and their associated analysis files, we could get a little more.
    
    The goal is to allow rapid iteration on the IPDL mapping and analysis
    record generation without having to go through a full mozilla-central
    lookup.
    
    Minimal test checks added.

--- tests/tests/mc-generated/dist/include/xpctest_bug809674.h
+++ tests/tests/mc-generated/dist/include/xpctest_bug809674.h
@@ -41,7 +41,7 @@ class NS_NO_VTABLE nsIXPCTestBug809674 : public nsISupports {
   JS_HAZ_CAN_RUN_SCRIPT NS_IMETHOD AddSubMulArgs(uint32_t x, uint32_t y, uint32_t *subOut, uint32_t *mulOut, JSContext* cx, uint32_t *_retval) = 0;
 
   /* [implicit_jscontext] jsval addVals (in jsval x, in jsval y); */
-  JS_HAZ_CAN_RUN_SCRIPT NS_IMETHOD AddVals(JS::HandleValue x, JS::HandleValue y, JSContext* cx, JS::MutableHandleValue _retval) = 0;
+  JS_HAZ_CAN_RUN_SCRIPT NS_IMETHOD AddVals(JS::Handle<JS::Value> x, JS::Handle<JS::Value> y, JSContext* cx, JS::MutableHandle<JS::Value> _retval) = 0;
 
   /* [implicit_jscontext] unsigned long methodNoArgs (); */
   JS_HAZ_CAN_RUN_SCRIPT NS_IMETHOD MethodNoArgs(JSContext* cx, uint32_t *_retval) = 0;
@@ -53,8 +53,8 @@ class NS_NO_VTABLE nsIXPCTestBug809674 : public nsISupports {
   JS_HAZ_CAN_RUN_SCRIPT NS_IMETHOD AddMany(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t x4, uint32_t x5, uint32_t x6, uint32_t x7, uint32_t x8, JSContext* cx, uint32_t *_retval) = 0;
 
   /* [implicit_jscontext] attribute jsval valProperty; */
-  JS_HAZ_CAN_RUN_SCRIPT NS_IMETHOD GetValProperty(JSContext* cx, JS::MutableHandleValue aValProperty) = 0;
-  JS_HAZ_CAN_RUN_SCRIPT NS_IMETHOD SetValProperty(JSContext* cx, JS::HandleValue aValProperty) = 0;
+  JS_HAZ_CAN_RUN_SCRIPT NS_IMETHOD GetValProperty(JSContext* cx, JS::MutableHandle<JS::Value> aValProperty) = 0;
+  JS_HAZ_CAN_RUN_SCRIPT NS_IMETHOD SetValProperty(JSContext* cx, JS::Handle<JS::Value> aValProperty) = 0;
 
   /* [implicit_jscontext] attribute unsigned long uintProperty; */
   JS_HAZ_CAN_RUN_SCRIPT NS_IMETHOD GetUintProperty(JSContext* cx, uint32_t *aUintProperty) = 0;
@@ -71,12 +71,12 @@ class NS_NO_VTABLE nsIXPCTestBug809674 : public nsISupports {
 #define NS_DECL_NSIXPCTESTBUG809674 \
   NS_IMETHOD AddArgs(uint32_t x, uint32_t y, JSContext* cx, uint32_t *_retval) override; \
   NS_IMETHOD AddSubMulArgs(uint32_t x, uint32_t y, uint32_t *subOut, uint32_t *mulOut, JSContext* cx, uint32_t *_retval) override; \
-  NS_IMETHOD AddVals(JS::HandleValue x, JS::HandleValue y, JSContext* cx, JS::MutableHandleValue _retval) override; \
+  NS_IMETHOD AddVals(JS::Handle<JS::Value> x, JS::Handle<JS::Value> y, JSContext* cx, JS::MutableHandle<JS::Value> _retval) override; \
   NS_IMETHOD MethodNoArgs(JSContext* cx, uint32_t *_retval) override; \
   NS_IMETHOD MethodNoArgsNoRetVal(JSContext* cx) override; \
   NS_IMETHOD AddMany(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t x4, uint32_t x5, uint32_t x6, uint32_t x7, uint32_t x8, JSContext* cx, uint32_t *_retval) override; \
-  NS_IMETHOD GetValProperty(JSContext* cx, JS::MutableHandleValue aValProperty) override; \
-  NS_IMETHOD SetValProperty(JSContext* cx, JS::HandleValue aValProperty) override; \
+  NS_IMETHOD GetValProperty(JSContext* cx, JS::MutableHandle<JS::Value> aValProperty) override; \
+  NS_IMETHOD SetValProperty(JSContext* cx, JS::Handle<JS::Value> aValProperty) override; \
   NS_IMETHOD GetUintProperty(JSContext* cx, uint32_t *aUintProperty) override; \
   NS_IMETHOD SetUintProperty(JSContext* cx, uint32_t aUintProperty) override; \
   NS_IMETHOD MethodWithOptionalArgc(uint8_t _argc) override; 
@@ -86,12 +86,12 @@ class NS_NO_VTABLE nsIXPCTestBug809674 : public nsISupports {
 #define NS_DECL_NON_VIRTUAL_NSIXPCTESTBUG809674 \
   nsresult AddArgs(uint32_t x, uint32_t y, JSContext* cx, uint32_t *_retval); \
   nsresult AddSubMulArgs(uint32_t x, uint32_t y, uint32_t *subOut, uint32_t *mulOut, JSContext* cx, uint32_t *_retval); \
-  nsresult AddVals(JS::HandleValue x, JS::HandleValue y, JSContext* cx, JS::MutableHandleValue _retval); \
+  nsresult AddVals(JS::Handle<JS::Value> x, JS::Handle<JS::Value> y, JSContext* cx, JS::MutableHandle<JS::Value> _retval); \
   nsresult MethodNoArgs(JSContext* cx, uint32_t *_retval); \
   nsresult MethodNoArgsNoRetVal(JSContext* cx); \
   nsresult AddMany(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t x4, uint32_t x5, uint32_t x6, uint32_t x7, uint32_t x8, JSContext* cx, uint32_t *_retval); \
-  nsresult GetValProperty(JSContext* cx, JS::MutableHandleValue aValProperty); \
-  nsresult SetValProperty(JSContext* cx, JS::HandleValue aValProperty); \
+  nsresult GetValProperty(JSContext* cx, JS::MutableHandle<JS::Value> aValProperty); \
+  nsresult SetValProperty(JSContext* cx, JS::Handle<JS::Value> aValProperty); \
   nsresult GetUintProperty(JSContext* cx, uint32_t *aUintProperty); \
   nsresult SetUintProperty(JSContext* cx, uint32_t aUintProperty); \
   nsresult MethodWithOptionalArgc(uint8_t _argc); 
@@ -100,12 +100,12 @@ class NS_NO_VTABLE nsIXPCTestBug809674 : public nsISupports {
 #define NS_FORWARD_NSIXPCTESTBUG809674(_to) \
   NS_IMETHOD AddArgs(uint32_t x, uint32_t y, JSContext* cx, uint32_t *_retval) override { return _to AddArgs(x, y, cx, _retval); } \
   NS_IMETHOD AddSubMulArgs(uint32_t x, uint32_t y, uint32_t *subOut, uint32_t *mulOut, JSContext* cx, uint32_t *_retval) override { return _to AddSubMulArgs(x, y, subOut, mulOut, cx, _retval); } \
-  NS_IMETHOD AddVals(JS::HandleValue x, JS::HandleValue y, JSContext* cx, JS::MutableHandleValue _retval) override { return _to AddVals(x, y, cx, _retval); } \
+  NS_IMETHOD AddVals(JS::Handle<JS::Value> x, JS::Handle<JS::Value> y, JSContext* cx, JS::MutableHandle<JS::Value> _retval) override { return _to AddVals(x, y, cx, _retval); } \
   NS_IMETHOD MethodNoArgs(JSContext* cx, uint32_t *_retval) override { return _to MethodNoArgs(cx, _retval); } \
   NS_IMETHOD MethodNoArgsNoRetVal(JSContext* cx) override { return _to MethodNoArgsNoRetVal(cx); } \
   NS_IMETHOD AddMany(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t x4, uint32_t x5, uint32_t x6, uint32_t x7, uint32_t x8, JSContext* cx, uint32_t *_retval) override { return _to AddMany(x1, x2, x3, x4, x5, x6, x7, x8, cx, _retval); } \
-  NS_IMETHOD GetValProperty(JSContext* cx, JS::MutableHandleValue aValProperty) override { return _to GetValProperty(cx, aValProperty); } \
-  NS_IMETHOD SetValProperty(JSContext* cx, JS::HandleValue aValProperty) override { return _to SetValProperty(cx, aValProperty); } \
+  NS_IMETHOD GetValProperty(JSContext* cx, JS::MutableHandle<JS::Value> aValProperty) override { return _to GetValProperty(cx, aValProperty); } \
+  NS_IMETHOD SetValProperty(JSContext* cx, JS::Handle<JS::Value> aValProperty) override { return _to SetValProperty(cx, aValProperty); } \
   NS_IMETHOD GetUintProperty(JSContext* cx, uint32_t *aUintProperty) override { return _to GetUintProperty(cx, aUintProperty); } \
   NS_IMETHOD SetUintProperty(JSContext* cx, uint32_t aUintProperty) override { return _to SetUintProperty(cx, aUintProperty); } \
   NS_IMETHOD MethodWithOptionalArgc(uint8_t _argc) override { return _to MethodWithOptionalArgc(_argc); } 
@@ -114,12 +114,12 @@ class NS_NO_VTABLE nsIXPCTestBug809674 : public nsISupports {
 #define NS_FORWARD_SAFE_NSIXPCTESTBUG809674(_to) \
   NS_IMETHOD AddArgs(uint32_t x, uint32_t y, JSContext* cx, uint32_t *_retval) override { return !_to ? NS_ERROR_NULL_POINTER : _to->AddArgs(x, y, cx, _retval); } \
   NS_IMETHOD AddSubMulArgs(uint32_t x, uint32_t y, uint32_t *subOut, uint32_t *mulOut, JSContext* cx, uint32_t *_retval) override { return !_to ? NS_ERROR_NULL_POINTER : _to->AddSubMulArgs(x, y, subOut, mulOut, cx, _retval); } \
-  NS_IMETHOD AddVals(JS::HandleValue x, JS::HandleValue y, JSContext* cx, JS::MutableHandleValue _retval) override { return !_to ? NS_ERROR_NULL_POINTER : _to->AddVals(x, y, cx, _retval); } \
+  NS_IMETHOD AddVals(JS::Handle<JS::Value> x, JS::Handle<JS::Value> y, JSContext* cx, JS::MutableHandle<JS::Value> _retval) override { return !_to ? NS_ERROR_NULL_POINTER : _to->AddVals(x, y, cx, _retval); } \
   NS_IMETHOD MethodNoArgs(JSContext* cx, uint32_t *_retval) override { return !_to ? NS_ERROR_NULL_POINTER : _to->MethodNoArgs(cx, _retval); } \
   NS_IMETHOD MethodNoArgsNoRetVal(JSContext* cx) override { return !_to ? NS_ERROR_NULL_POINTER : _to->MethodNoArgsNoRetVal(cx); } \
   NS_IMETHOD AddMany(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t x4, uint32_t x5, uint32_t x6, uint32_t x7, uint32_t x8, JSContext* cx, uint32_t *_retval) override { return !_to ? NS_ERROR_NULL_POINTER : _to->AddMany(x1, x2, x3, x4, x5, x6, x7, x8, cx, _retval); } \
-  NS_IMETHOD GetValProperty(JSContext* cx, JS::MutableHandleValue aValProperty) override { return !_to ? NS_ERROR_NULL_POINTER : _to->GetValProperty(cx, aValProperty); } \
-  NS_IMETHOD SetValProperty(JSContext* cx, JS::HandleValue aValProperty) override { return !_to ? NS_ERROR_NULL_POINTER : _to->SetValProperty(cx, aValProperty); } \
+  NS_IMETHOD GetValProperty(JSContext* cx, JS::MutableHandle<JS::Value> aValProperty) override { return !_to ? NS_ERROR_NULL_POINTER : _to->GetValProperty(cx, aValProperty); } \
+  NS_IMETHOD SetValProperty(JSContext* cx, JS::Handle<JS::Value> aValProperty) override { return !_to ? NS_ERROR_NULL_POINTER : _to->SetValProperty(cx, aValProperty); } \
   NS_IMETHOD GetUintProperty(JSContext* cx, uint32_t *aUintProperty) override { return !_to ? NS_ERROR_NULL_POINTER : _to->GetUintProperty(cx, aUintProperty); } \
   NS_IMETHOD SetUintProperty(JSContext* cx, uint32_t aUintProperty) override { return !_to ? NS_ERROR_NULL_POINTER : _to->SetUintProperty(cx, aUintProperty); } \
   NS_IMETHOD MethodWithOptionalArgc(uint8_t _argc) override { return !_to ? NS_ERROR_NULL_POINTER : _to->MethodWithOptionalArgc(_argc); } 
