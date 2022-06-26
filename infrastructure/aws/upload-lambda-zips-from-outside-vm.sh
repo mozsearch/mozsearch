fb29ac7f83483f01c14b6481b02f13a4fd49bef9
Andrew Sutherland <asutherland@asutherland.org>
2022-06-26 17:50:25 -0400

    Bug 1766925 - release3 and release5 cron job support/docs

--- infrastructure/aws/upload-lambda-zips-from-outside-vm.sh
+++ infrastructure/aws/upload-lambda-zips-from-outside-vm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # This script is intended to be run outside the VM with AWS creds active after
-# running `build-lambda-zips-from-inside-vm.sh`.  There should be 3
+# running `build-lambda-zips-from-inside-vm.sh`.  There should be 5
 # `lambda-releaseN.zip` files in the root of the MOZSEARCH dir and you should
 # be running the script from that root.  See `aws.md` for more details.
 
@@ -21,6 +21,16 @@ aws lambda update-function-code \
 rm lambda-release2.zip
 
 aws lambda update-function-code \
+  --function-name start-release3-indexer \
+  --zip-file fileb://lambda-release3.zip
+rm lambda-release3.zip
+
+aws lambda update-function-code \
   --function-name start-release4-indexer \
   --zip-file fileb://lambda-release4.zip
 rm lambda-release4.zip
+
+aws lambda update-function-code \
+  --function-name start-release5-indexer \
+  --zip-file fileb://lambda-release5.zip
+rm lambda-release5.zip
