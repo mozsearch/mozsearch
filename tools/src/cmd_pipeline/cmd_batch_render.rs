1fe2066bf465f35a0643e1ec23854ef5ca9dd6d9
Andrew Sutherland <asutherland@asutherland.org>
2022-08-12 18:46:49 -0400

    Bug 1783761 - extract out per_file_info.rs from output-file.rs
    For directory consumption, we're going to want to have a more formal
    data structure hierarchy for per-file info.  The plan is to then
    serialize the aggregated information into the "description" files which
    will become JSON.
    
    The derivation of this information potentially might end up moving
    into the crossref phase in the future.

---
+++ tools/src/cmd_pipeline/cmd_batch_render.rs
@@ -0,0 +1,40 @@
+use async_trait::async_trait;
+use serde_json::{to_value};
+use structopt::StructOpt;
+
+use super::{interface::{JsonValue, PipelineCommand, PipelineValues}, builder::build_pipeline_graph};
+use crate::{
+    abstract_server::{AbstractServer, Result}, query::chew_query::chew_query,
+};
+
+#[derive(Debug, StructOpt)]
+pub struct BatchRender {
+    /// Preconfigured rendering task.  This could be an enum or sub-command, but
+    /// for now we're just going for strings.
+    task: String,
+}
+
+#[derive(Debug)]
+pub struct BatchRenderCommand {
+    pub args: BatchRender,
+}
+
+#[async_trait]
+impl PipelineCommand for BatchRenderCommand {
+    async fn execute(
+        &self,
+        server: &Box<dyn AbstractServer + Send + Sync>,
+        _input: PipelineValues,
+    ) -> Result<PipelineValues> {
+        let pipeline_plan = chew_query(&self.args.query)?;
+
+        if self.args.dump_pipeline {
+            return Ok(PipelineValues::JsonValue(JsonValue { value: to_value(pipeline_plan)? }));
+        }
+
+        let graph = build_pipeline_graph(server.clonify(), pipeline_plan)?;
+
+        let result = graph.run(true).await;
+        result
+    }
+}
