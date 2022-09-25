05bd9c3c3413a8011f2ef9669cf344f6655444bf
Andrew Sutherland <asutherland@asutherland.org>
2022-09-25 12:40:33 -0400

    Bug 1792322 - Handle `export default` by not handling it.

--- tests/tests/files/root-module.mjs
+++ tests/tests/files/root-module.mjs
@@ -2,4 +2,11 @@ import { moduleConst, moduleFunc, ModuleClass } from "./imported-module.mjs";
 import { default as aliasedDefault } from "./imported-module.mjs";
 import * as importedModule from "./imported-module.mjs";
 
+function exportedAsDefaultAsReference() {
+    return 5;
+}
+
+// This should generate a failure.
+export default exportedAsDefaultAsReference;
+
 const rootModuleConst = 10;
