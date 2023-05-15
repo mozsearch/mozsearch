5a4a4dbee9b7ba2dfa3666654f355bd8f222ec2b
Botond Ballo <botond@mozilla.com>
2023-05-15 19:20:57 -0400

    Bug 1833272 - Add testcase

--- tests/tests/files/bug1781178.cpp
+++ tests/tests/files/bug1781178.cpp
@@ -2,7 +2,7 @@ template <typename> struct Point {
   bool IsThereOne();
 };
 
-template <typename>
+template <typename T>
 struct Foo {
   void Simple();
   static void Static();
@@ -16,6 +16,9 @@ struct Foo {
   void Bar() {
     Point<float> p;
     Project(p);
+
+    Point<T> tp;
+    this->Project(tp);
 
     this->Simple();
   }
