75348697442477cda58bfaf789d3a396985f936e
Andrew Sutherland <asutherland@asutherland.org>
2023-08-11 13:11:52 -0400

    Address graphviz-rust escaping issue, add test.
    graphviz-rust has an `esc` macro that's a bit misleading as it does not
    actually do any escaping of the value, it just marks that the value
    should be wrapped in double-quotes, which is an interesting choice but
    probably one that won't be changed anytime soon.
    
    I've manually inspected and updated the various flows to graphviz-rust
    reps, but it's complicated by the 2-stage rendering approach of compile
    (which populates the HierarchicalLayoutActions which can contain ids)
    followed by render.  Also that we need to sanitize the port identifiers
    to a stronger standard than node identifiers because they end up as
    part of the node identifiers.
    
    Longer term it would probably be preferable to add some newtype
    wrappers so that as soon as we move into graphviz space we have
    explicit types that explicitly state whether they've undergone escaping
    and potentially have helper methods that produce the appropriate
    graphviz-rust types if it reads acceptably.

---
+++ tests/tests/files/tricky_symbol_names.cpp
@@ -0,0 +1,14 @@
+#include <cstddef>
+#include <cstdio>
+
+constexpr auto operator""_argggh(const char* aStr, std::size_t aLen) {
+  return aStr;
+}
+
+// This can't be named main() or we'll mess up an existing graph test, whoops.
+int use_the_argggh_operator() {
+    const char *blah = "blah"_argggh;
+    printf("%s\n", blah);
+
+    return 0;
+}
