378a1227b10b75f4db986acadd95a259530f230d
Tooru Fujisawa <arai_a@mac.com>
2024-05-27 01:20:09 +0900

    Make analysis types generic over Ustr/String and use String variant from css_analyzer.

--- tools/src/css_analyzer.rs
+++ tools/src/css_analyzer.rs
@@ -1,8 +1,8 @@
 use cssparser;
-use ustr::{Ustr, ustr};
 
 use crate::file_format::analysis::{
-    AnalysisKind, AnalysisSource, AnalysisTarget, LineRange, Location, SourceRange, SourceTag, TargetTag, WithLocation
+    AnalysisKind, AnalysisSource, AnalysisTarget, LineRange, Location,
+    SourceRange, SourceTag, TargetTag, WithLocation
 };
 
 // NOTE: This does the same as analysis_manglings::mangle_file without regex
@@ -37,7 +37,7 @@ fn to_loc(first_line: u32,
     }
 }
 
-fn to_source(loc: Location, syntax: Vec<Ustr>, pretty: Ustr, sym: Ustr) -> WithLocation<AnalysisSource> {
+fn to_source(loc: Location, syntax: Vec<String>, pretty: String, sym: String) -> WithLocation<AnalysisSource::<String>> {
     WithLocation {
         data: AnalysisSource {
             source: SourceTag::Source,
@@ -54,15 +54,15 @@ fn to_source(loc: Location, syntax: Vec<Ustr>, pretty: Ustr, sym: Ustr) -> WithL
     }
 }
 
-fn to_target(loc: Location, kind: AnalysisKind, pretty: Ustr, sym: Ustr) -> WithLocation<AnalysisTarget> {
+fn to_target(loc: Location, kind: AnalysisKind, pretty: String, sym: String) -> WithLocation<AnalysisTarget::<String>> {
     WithLocation {
         data: AnalysisTarget {
             target: TargetTag::Target,
             kind: kind,
             pretty: pretty,
             sym: sym,
-            context: ustr(""),
-            contextsym: ustr(""),
+            context: "".to_string(),
+            contextsym: "".to_string(),
             peek_range: LineRange {
                 start_lineno: 0,
                 end_lineno: 0,
@@ -75,7 +75,9 @@ fn to_target(loc: Location, kind: AnalysisKind, pretty: Ustr, sym: Ustr) -> With
 
 fn analyze_css_block<F>(input: &mut cssparser::Parser, first_line: u32,
                         is_var: bool, callback: &mut F)
-where F: FnMut(String) {
+where
+    F: FnMut(String)
+{
     use cssparser::Token::*;
     let mut start = input.current_source_location();
     while let Ok(token) = input.next_including_whitespace_and_comments().cloned() {
@@ -86,13 +88,13 @@ where F: FnMut(String) {
             Ident(name) => {
                 if name.starts_with("--") {
                     let loc = to_loc(first_line, &start, &end);
-                    let source_pretty = ustr(format!("property {}", name.as_ref()).as_str());
-                    let target_pretty = ustr(name.as_ref());
-                    let sym = ustr(format!("CSSPROP_{}", mangle_name(name.as_ref())).as_str());
+                    let source_pretty = format!("property {}", name.as_ref());
+                    let target_pretty = name.to_string();
+                    let sym = format!("CSSPROP_{}", mangle_name(name.as_ref()));
                     let (syntax, kind) = if is_var {
-                        (vec![ustr("use"), ustr("cssprop")], AnalysisKind::Use)
+                        (vec!["use".to_string(), "cssprop".to_string()], AnalysisKind::Use)
                     } else {
-                        (vec![ustr("def"), ustr("cssprop")], AnalysisKind::Def)
+                        (vec!["def".to_string(), "cssprop".to_string()], AnalysisKind::Def)
                     };
 
                     let source = to_source(loc.clone(), syntax, source_pretty, sym.clone());
@@ -104,10 +106,10 @@ where F: FnMut(String) {
             QuotedString(s) | UnquotedUrl(s) => {
                 if s.starts_with("chrome://") || s.starts_with("resource://") {
                     let loc = to_loc(first_line, &start, &end);
-                    let source_pretty = ustr(format!("file {}", s.as_ref()).as_str());
-                    let target_pretty = ustr(s.as_ref());
-                    let sym = ustr(format!("URL_{}", mangle_name(s.as_ref())).as_str());
-                    let syntax = vec![ustr("use"), ustr("file")];
+                    let source_pretty = format!("file {}", s.as_ref());
+                    let target_pretty = s.to_string();
+                    let sym = format!("URL_{}", mangle_name(s.as_ref()));
+                    let syntax = vec!["use".to_string(), "file".to_string()];
                     let kind = AnalysisKind::Use;
 
                     let source = to_source(loc.clone(), syntax, source_pretty, sym.clone());
@@ -141,7 +143,9 @@ where F: FnMut(String) {
 
 pub fn analyze_css<F>(path: String, first_line: u32,
                   text: String, callback: &mut F)
-where F: FnMut(String) {
+where
+    F: FnMut(String)
+{
     let mut input = cssparser::ParserInput::new(text.as_str());
     let mut input = cssparser::Parser::new(&mut input);
 
@@ -151,8 +155,8 @@ where F: FnMut(String) {
             col_start: 0,
             col_end: 0,
         };
-        let pretty = ustr(format!("file {}", path).as_str());
-        let sym = ustr(format!("FILE_{}", mangle_name(path.as_str())).as_str());
+        let pretty = format!("file {}", path);
+        let sym = format!("FILE_{}", mangle_name(path.as_str()));
         let kind = AnalysisKind::Def;
         let target = to_target(loc, kind, pretty, sym);
 
