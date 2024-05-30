1372cb3e4d07581b6332d70aa28ec21435213a3a
Tooru Fujisawa <arai_a@mac.com>
2024-05-31 05:09:29 +0900

    Bug 1895653 - Part 1: Support URLs in dynamic import.

--- tests/tests/files/urlmap/root.js
+++ tests/tests/files/urlmap/root.js
@@ -9,3 +9,8 @@ ChromeUtils.defineESModuleGetters(lazy, {
 
 window.open("chrome://global/content/test/chrome1.html");
 window.open("resource://test/resource1.html");
+
+async function f() {
+  const { chrome1 } = await import("chrome://global/content/test/chrome2.mjs");
+  const { resource1 } = await import("resource://test/resource2.mjs");
+}
