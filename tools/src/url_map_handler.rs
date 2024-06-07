15a31099067c27890aceb80d2a1cb34f5452783a
Tooru Fujisawa <arai_a@mac.com>
2024-06-08 01:14:00 +0900

    Bug 1900823 - Part 3: Fix non-HEAD rendering not to unwrap URL_MAP_PATH with None value

--- tools/src/url_map_handler.rs
+++ tools/src/url_map_handler.rs
@@ -12,7 +12,7 @@ pub fn set_url_map_path(path: &str) {
 pub fn get_file_paths_for_url(url: &str) -> Option<Vec<URLMapItem>> {
     lazy_static! {
         static ref URL_MAP: URLMap = unsafe {
-            read_url_map(URL_MAP_PATH.as_ref().unwrap().as_str())
+            read_url_map(URL_MAP_PATH.as_ref().map(|s| s.as_str()))
         };
     }
 
