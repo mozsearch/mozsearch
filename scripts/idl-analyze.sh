d620a33e10df92c87e36f215e31b41ec5f3797ca
Tooru Fujisawa <arai_a@mac.com>
2024-06-10 08:39:01 +0900

    Bug 1416899 - Part 4: Index WebIDL files

--- scripts/idl-analyze.sh
+++ scripts/idl-analyze.sh
@@ -13,17 +13,18 @@ fi
 CONFIG_FILE=$(realpath $1)
 TREE_NAME=$2
 
-if [ -f "${FILES_ROOT}/xpcom/idl-parser/xpidl/xpidl.py" ]; then
-    # The tree we're processing has an xpidl.py, so let's use that
+if [ -f "${FILES_ROOT}/xpcom/idl-parser/xpidl/xpidl.py" -a \
+     -f "${FILES_ROOT}/dom/bindings/parser/WebIDL.py" ]; then
+    # The tree we're processing has IDL parsers, so let's use that
     TREE_PYMODULES="/tmp/pymodules-${TREE_NAME}"
     PULL_FROM_MC=0
 else
-    # The tree we're processing doesn't have an xpidl.py, so we'll pull m-c's copy with wget
+    # The tree we're processing doesn't have IDL parsers, so we'll pull m-c's copy with wget
     TREE_PYMODULES="/tmp/pymodules"
     PULL_FROM_MC=1
 fi
 
-# Delete the temp dir if xpidl.py is older than a day (in minutes to avoid
+# Delete the temp dir if IDL parsers are older than a day (in minutes to avoid
 # quantization weirdness).  We'll also try and delete the dir if the file just
 # doesn't exist, which also means if the directory doesn't exist.  (We could
 # have instead done `-mmin +1440` for affirmative confirmation it's old, but
@@ -39,8 +40,10 @@ if [ ! -d "${TREE_PYMODULES}" ]; then
     pushd "${TREE_PYMODULES}"
     if [ $PULL_FROM_MC -eq 0 ]; then
         cp "${FILES_ROOT}/xpcom/idl-parser/xpidl/xpidl.py" ./
+        cp "${FILES_ROOT}/dom/bindings/parser/WebIDL.py" ./
     else
         wget "https://hg.mozilla.org/mozilla-central/raw-file/tip/xpcom/idl-parser/xpidl/xpidl.py"
+        wget "https://hg.mozilla.org/mozilla-central/raw-file/tip/dom/bindings/parser/WebIDL.py"
     fi
     mkdir ply
     pushd ply
@@ -60,4 +63,8 @@ export PYTHONPATH="${TREE_PYMODULES}"
 cat $INDEX_ROOT/idl-files | \
     parallel $MOZSEARCH_PATH/scripts/idl-analyze.py \
     $INDEX_ROOT $FILES_ROOT/{} ">" $INDEX_ROOT/analysis/{}
+
+cat $INDEX_ROOT/webidl-files | \
+    $MOZSEARCH_PATH/scripts/webidl-analyze.py \
+    $INDEX_ROOT $FILES_ROOT $INDEX_ROOT/analysis /tmp
 echo $?
