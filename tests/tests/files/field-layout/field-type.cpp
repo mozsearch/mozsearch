2c01091558a7473de02cdbdf8470cdf792e41004
Tooru Fujisawa <arai_a@mac.com>
2024-06-14 08:42:07 +0900

    Bug 1900830 - Part 4: Add more JS types to ontology mapping

--- tests/tests/files/field-layout/field-type.cpp
+++ tests/tests/files/field-layout/field-type.cpp
@@ -1,5 +1,55 @@
 #include <vector>
+#include <stddef.h>
 #include <stdint.h>
+
+class JSObject {
+};
+
+namespace JS {
+
+class TestAllocPolicy {
+};
+
+
+template <typename T>
+class Rooted {
+};
+
+template <typename T, size_t MinInlineCapacity = 0,
+          typename AllocPolicy = TestAllocPolicy>
+class GCVector {
+};
+
+template <typename T, typename AllocPolicy = TestAllocPolicy>
+class StackGCVector : public GCVector<T, 8, AllocPolicy> {
+};
+
+template <typename T>
+class RootedVector : public Rooted<StackGCVector<T>> {
+};
+
+class alignas(8) Value {
+ private:
+  uint64_t asBits_;
+};
+
+
+template <typename Key, typename Value>
+class GCHashMap {
+};
+
+}
+
+namespace js {
+
+class TestCheck {
+};
+
+template <typename Check, typename T>
+class ProtectedDataNoCheckArgs {
+};
+
+}
 
 namespace field_layout {
 
@@ -30,6 +80,10 @@ struct S {
   Container1<Type1> template_field_1;
   Container2<Type1, Enum1::No> template_field_2;
   std::vector<Type1> vector_field;
+  JS::Rooted<JS::Value> rooted_field;
+  JS::RootedVector<JS::Value> rooted_vec_field;
+  JS::GCHashMap<JS::Value, JSObject*> hash_map_field;
+  js::ProtectedDataNoCheckArgs<js::TestCheck,  JS::Value> protected_field;
 };
 
 S f() {
