99892ac21dca257e74123138b8fa7f6b6d5891aa
Tooru Fujisawa <arai_a@mac.com>
2024-07-28 07:09:24 +0900

    Bug 1904320 - Part 1: Add test for WebIDL bindings

---
+++ tests/tests/files/webidl/bindings/include/BindingTestBinding.h
@@ -0,0 +1,22 @@
+#include <stdint.h>
+
+namespace mozilla {
+namespace dom {
+
+namespace BindingTest_Binding {
+
+static const uint32_t CONST_1 = 10;
+
+} // BindingTest_Binding
+
+struct BindingTestDict {
+  unsigned long mProp1;
+};
+
+enum class BindingTestEnum {
+  Variant1,
+  Variant2,
+};
+
+} // namespace dom
+} // namespace mozilla
