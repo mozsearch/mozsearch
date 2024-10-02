e986b8190942a02f2ed4e250a68bd0bfbb2e904c
Nicolas Guichard <nicolas.guichard@kdab.com>
2024-10-02 18:38:26 +0200

    Bug 1833695: reset DeclContext stack when analysing out-of-line templated function
    Without this, OutOfLineTemplateShouldntHaveContextSym had two
    definition target records that disagreed on the contextsym:
    1. The first one was generated when traversing the declaration, because
       it forwards to the definition if out-of-line (see comment in
       TraverseFunctionDecl).
       We didn't reset CurDeclContext, so it looked as-if the definition
       appeared inside the declaration, leading to a bogus contextsym
       (pointing to the function itself).
    2. The second one was generated later on when traversing the definition
       itself. This one had a correct contextsym (ie. none).
    
    This introduces a ValueRollback helper to make sure we don't forget to
    rollback CurDeclContext afterwards. (inspired by QScopedValueRollback)

---
+++ tests/tests/files/templates7.cpp
@@ -0,0 +1,25 @@
+// Testcase proposed by Botond on https://bugzilla.mozilla.org/show_bug.cgi?id=1833695
+
+struct Theme {
+  void CreateWebRenderCommandsForWidget() {
+    OutOfLineTemplateShouldntHaveContextSym(0);
+    OutOfLineShouldntHaveContextSym(0);
+    InlineTemplateShouldHaveContextSym(0);
+    InlineShouldHaveContextSym(0);
+  }
+
+  template<typename T>
+  void OutOfLineTemplateShouldntHaveContextSym(T);
+
+  void OutOfLineShouldntHaveContextSym(int);
+
+  template<typename T>
+  inline void InlineTemplateShouldHaveContextSym(T) {}
+
+  inline void InlineShouldHaveContextSym(int) {}
+};
+
+template <typename T>
+void Theme::OutOfLineTemplateShouldntHaveContextSym(T) {}
+
+void Theme::OutOfLineShouldntHaveContextSym(int) {}
