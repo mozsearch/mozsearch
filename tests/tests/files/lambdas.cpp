cfd824fd5acfc9d05e6e2e1a91850a90bf4ae177
Nicolas Guichard <nicolas.guichard@kdab.com>
2024-10-06 10:24:20 +0200

    Bug 1922888 - Fix IndexConsumer::getContext(SourceLocation) when Ctxt->Decl is null.
    Bug 1856762 added support for traversing the implicit declarations
    created for lambdas. When traversing a lambda, a new context is pushed
    to the AutoSetContext stack to set the VisitImplicit flag. This context
    has Decl == nullptr because the lambda is not a NamedDecl.
    
    I modified IndexConsumer::getContext(Decl *D) to support that case, but
    forgot to modify getContext(SourceLocation Loc) to support it too. In
    the added test case, the non-catch-all captures triggered a crash.

--- tests/tests/files/lambdas.cpp
+++ tests/tests/files/lambdas.cpp
@@ -13,4 +13,11 @@ void test() {
 
     lambda(Struct0{});
     lambda(Struct1{});
+
+    const auto capture_all_by_reference = [&]{ (void)lambda; };
+    const auto capture_all_by_value = [=]{ (void)lambda; };
+    const auto capture_one_by_reference = [&lambda]{ (void)lambda; };
+    const auto capture_one_by_value = [lambda]{ (void)lambda; };
+    const auto capture_by_named_reference = [&lambda = lambda]{ (void)lambda; };
+    const auto capture_by_named_value = [lambda = lambda]{ (void)lambda; };
 }
