252520e2fd8fcee08b0000ef85e07ce382742c1c
Tooru Fujisawa <arai_a@mac.com>
2024-10-13 03:52:04 +0900

    Bug 1910314 - Part 2: URL-encode links in search result

--- tests/webtest/test_SpaceInFilename.js
+++ tests/webtest/test_SpaceInFilename.js
@@ -12,4 +12,9 @@ add_task(async function test_SpaceInFilename() {
       content.textContent.includes("Definitions (SymbolInFilenameWithSpace) (1 lines") &&
       content.textContent.includes("var SymbolInFilenameWithSpace"),
     "symbol in file with space in filename matches as definition");
+
+  const links = frame.contentDocument.querySelectorAll(".result-head a");
+  is(links.length, 2);
+  is(links[1].getAttribute("href"), "/tests/source/js/with%20space.js",
+     "The space in the href should be escaped");
 });
