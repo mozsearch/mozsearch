d152103d1477ab177efc2524a74a630c01cd4d0c
Nicolas Guichard <nicolas.guichard@kdab.com>
2024-10-30 16:36:55 +0100

    Bug 1453506 - Forward type-dependent news to template call locations.
    This provides a way to make type-dependent statements appear as-if they
    were located at the template call site.
    
    During the analysis of the template, this first registers all type-dependent
    function call locations in ForwardedTemplateLocations (we don't have more
    information at this point).
    
    Then when analyzing the template instantiations, we link the actual
    type-resolved statements to the template instantiation.
    
    Then when emitting the analysis data for a template call, we also (recursively)
    emit all the analysis data for the forwarded items at the call site.

---
+++ tests/tests/files/MakeUnique.cpp
@@ -0,0 +1,51 @@
+#include <memory>
+
+struct StructUsedInTypeDependentNew0 {
+  StructUsedInTypeDependentNew0() {}
+};
+
+struct StructUsedInTypeDependentNew1 {
+  StructUsedInTypeDependentNew1() {}
+};
+
+struct StructUsedInTypeIndependentNew {
+  StructUsedInTypeIndependentNew() {}
+};
+
+template <typename T, typename... Args> std::unique_ptr<T> MakeUniqueWithIndex(int i, Args &&...args) {
+  const auto _ = std::unique_ptr<StructUsedInTypeIndependentNew>{new StructUsedInTypeIndependentNew()};
+  return std::unique_ptr<T>{new T(std::forward<Args>(args)...)};
+}
+
+template <typename T, typename... Args> std::unique_ptr<T> MakeUnique(Args &&...args) {
+  const auto _ = std::unique_ptr<StructUsedInTypeIndependentNew>{new StructUsedInTypeIndependentNew()};
+  return MakeUniqueWithIndex<T>(0, std::forward<Args>(args)...);
+}
+
+template <typename T, typename... Args> std::unique_ptr<T> RecursiveMakeUnique(Args &&...args) {
+  const auto _ = RecursiveMakeUnique<T>(std::forward<Args>(args)...);
+  return MakeUnique<T>(std::forward<Args>(args)...);
+}
+
+template <typename T, typename... Args> std::unique_ptr<T> MakeUniqueWithLambda(Args &&...args) {
+  return std::unique_ptr<T>{[t = new T()]{ return t; }()};
+}
+
+void test() {
+  const auto a = MakeUniqueWithIndex<StructUsedInTypeDependentNew0>(0);
+  const auto b = MakeUniqueWithIndex<StructUsedInTypeDependentNew0>(0);
+  const auto c = MakeUniqueWithIndex<StructUsedInTypeDependentNew1>(0);
+  const auto d = MakeUniqueWithIndex<StructUsedInTypeDependentNew1>(0);
+  const auto e = MakeUnique<StructUsedInTypeDependentNew0>();
+  const auto f = MakeUnique<StructUsedInTypeDependentNew0>();
+  const auto g = MakeUnique<StructUsedInTypeDependentNew1>();
+  const auto h = MakeUnique<StructUsedInTypeDependentNew1>();
+  const auto i = RecursiveMakeUnique<StructUsedInTypeDependentNew0>();
+  const auto j = RecursiveMakeUnique<StructUsedInTypeDependentNew0>();
+  const auto k = RecursiveMakeUnique<StructUsedInTypeDependentNew1>();
+  const auto l = RecursiveMakeUnique<StructUsedInTypeDependentNew1>();
+  const auto m = MakeUniqueWithLambda<StructUsedInTypeDependentNew0>();
+  const auto n = MakeUniqueWithLambda<StructUsedInTypeDependentNew0>();
+  const auto o = MakeUniqueWithLambda<StructUsedInTypeDependentNew1>();
+  const auto p = MakeUniqueWithLambda<StructUsedInTypeDependentNew1>();
+}
