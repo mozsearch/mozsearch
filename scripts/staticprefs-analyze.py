16979dd528410544c5e85ed1fd606fd677e4d2a8
Tooru Fujisawa <arai_a@mac.com>
2024-11-04 15:31:21 +0900

    Bug 1699048 - Part 2: Support JS::Prefs in StaticPrefs.

--- scripts/staticprefs-analyze.py
+++ scripts/staticprefs-analyze.py
@@ -39,11 +39,18 @@ def read_cpp_analysis_one(path, cpp_symbols):
 
         sym = j['sym']
         m = re.match('.+StaticPrefs[0-9]+(.+)Ev$', sym)
-        if not m:
-            continue
+        if m:
+            id = m.group(1)
+        else:
+            m = re.match('.+JS5Prefs[0-9]+(.+)Ev$', sym)
+            if m:
+                id = f'javascript_options_{m.group(1)}'
+            else:
+                continue
 
-        id = m.group(1)
-        cpp_symbols[id] = sym
+        if id not in cpp_symbols:
+            cpp_symbols[id] = set()
+        cpp_symbols[id].add(sym)
 
 
 def read_cpp_analysis(ns, yaml_path, bindings_local_path, analysis_root):
@@ -69,15 +76,27 @@ def read_cpp_analysis(ns, yaml_path, bindings_local_path, analysis_root):
                 p = os.path.join(generated_dir, name, header_local_path)
                 read_cpp_analysis_one(p, cpp_symbols)
 
+        if ns == 'javascript':
+            # Load SpiderMonkey-specific binding.
+            header_local_path = 'js/public/PrefsGenerated.h'
+
+            p = os.path.join(generated_dir, header_local_path)
+            read_cpp_analysis_one(p, cpp_symbols)
+
+            for name in os.listdir(generated_dir):
+                if name.startswith('__'):
+                    p = os.path.join(generated_dir, name, header_local_path)
+                    read_cpp_analysis_one(p, cpp_symbols)
+
     return cpp_symbols
 
 
 pref_bindings_map = {}
 
 
-def get_cpp_symbol(name, yaml_path, bindings_local_path, analysis_root):
-    '''Get the corresponding binding C++ symbol for the preference name.
-    Returns None if no binding is found.'''
+def get_cpp_symbols(name, yaml_path, bindings_local_path, analysis_root):
+    '''Get the corresponding binding C++ symbols for the preference name.
+    Returns an empty list if no binding is found.'''
     ns = name.split('.')[0]
 
     if ns in pref_bindings_map:
@@ -89,7 +108,7 @@ def get_cpp_symbol(name, yaml_path, bindings_local_path, analysis_root):
     id = mk_id(name)
 
     if id not in cpp_symbols:
-        return None
+        return []
 
     return cpp_symbols[id]
 
@@ -115,8 +134,8 @@ def process_file(yaml_path, bindings_local_path, files_root, analysis_root):
 
                 slots = []
 
-                cpp_sym = get_cpp_symbol(name, yaml_path, bindings_local_path, analysis_root)
-                if cpp_sym is not None:
+                cpp_syms = get_cpp_symbols(name, yaml_path, bindings_local_path, analysis_root)
+                for cpp_sym in cpp_syms:
                     slots.append({
                         'slotKind': 'getter',
                         'slotLang': 'cpp',
