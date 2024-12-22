9ec3c6b0bd7c973c22b42229997387523d1d38aa
Nicolas Guichard <nicolas.guichard@kdab.com>
2024-12-22 18:14:16 +0100

    Space savings: install nodejs18 from Ubuntu repos instead of using mise.
    As hinted in b83a0b65 wer don't really need mise.

--- infrastructure/indexer-provision.sh
+++ infrastructure/indexer-provision.sh
@@ -17,40 +17,18 @@ sudo apt-get install -y ninja-build
 # the update process.
 cargo install cargo-insta
 
-# To help install node.js and similar, we install mise, a rust-based "asdf"
-# alternative, which if you don't know what "asdf" is, but know what "nvm" is,
-# it's basically a super-nvm for multiple languages, etc.  We use the install
-# method documented at https://mise.jdx.dev/getting-started.html#cargo but there
-# are a bunch of other options.
-#
-# The core rationale here is that I've locally been using "nvm" for node.js
-# purposes for a while now and it's been a much better experience than trying to
-# use debian/ubuntu distro-provided versions of node, and in particular can be
-# invaluable when trying to just get things to work when packages are involved
-# that may involve native modules/libraries which can make it hard to uniformly
-# use the latest revision.  I'm somewhat hopeful that
-#
-# We are currently installing an older version of mise because as of 2014-12-15
-# rust nightly has problems with usage-lib related to miette.  The version
-# 2024.10.1 was specifically chosen because it was the version we were using on
-# the last successful provision.  The include graph for this seems potentially
-# way more than we need to be able to run node, so I think we will want to drop
-# this dep in the future absent some very good reason.
-cargo install mise@2024.10.1
-
 # Install node.js for scip-typescript; github lists v18 and v20 as supported;
 # we are sticking with v18 for now because currently all the invocations
 # hardcode v18 as well; that will need to be addressed.
-SCIP_NODEJS_VERSION=nodejs@18
-mise install ${SCIP_NODEJS_VERSION}
+sudo apt install -y npm
 
 # Install scip-typescript under node.js v18
-mise exec ${SCIP_NODEJS_VERSION} -- npm install -g @sourcegraph/scip-typescript
+sudo npm install -g @sourcegraph/scip-typescript
 
 # Install scip-python under node.js v18 as well
-#mise exec ${SCIP_NODEJS_VERSION} -- npm install -g @sourcegraph/scip-python
+#npm install -g @sourcegraph/scip-python
 # To get my fix https://github.com/sourcegraph/scip-python/pull/150
-mise exec ${SCIP_NODEJS_VERSION} -- npm install -g @asutherland/scip-python
+sudo npm install -g @asutherland/scip-python
 
 # Install a JDK and Coursier.
 # v21 is currently the most recent available version of Ubuntu 24.04 (and v19 was
