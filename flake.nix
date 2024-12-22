9e06bd91646d66f74e496f299eecc03efecd1e29
Nicolas Guichard <nicolas.guichard@kdab.com>
2024-12-22 21:02:33 +0100

    Nix Flake: Add nix-community.cachix.org substituter for Fenix
    Nix can rebuild everything locally, but has caches called substituters
    to avoid rebuilds. When installing Nix, a default substituter is setup
    for nixpkgs. The Rust toolchain used here does not come from nixpkgs
    but from https://github.com/nix-community/fenix as it provides a Rust
    toolchain to target wasm.
    
    When first running a build, Nix will ask whether the user trusts the
    suggested substituter or not. This can be forced with the
    --accept-flake-config flag.

--- flake.nix
+++ flake.nix
@@ -8,6 +8,11 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = ["https://nix-community.cachix.org"];
+    extra-trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+  };
+
   outputs = {
     self,
     nixpkgs,
