549e5cee06826340f2d51cc66cad348a116dfc6e
Nicolas Guichard <nicolas.guichard@kdab.com>
2025-07-31 10:14:11 +0200

    Use xpidl and webidl parsers from the tree itself if available.
    They are unstable and for instance the esr140 and nightly tree disagree
    on how to define async iterables in WebIDL.
    
    This shadows the existing Python modules if they are already installed.

--- scripts/idl-analyze.sh
+++ scripts/idl-analyze.sh
@@ -23,6 +23,25 @@ fi
 PYMODULES="$HOME/pymodules"
 export PYTHONPATH="${PYMODULES}${PYTHONPATH:+:${PYTHONPATH}}"
 
+# Use xpidl and webidl parsers from the tree itself if available.
+# They are unstable and for instance the esr140 and nightly tree disagree on how to define async iterables in WebIDL.
+if [ -f "${FILES_ROOT}/xpcom/idl-parser/xpidl/xpidl.py" -a \
+     -f "${FILES_ROOT}/dom/bindings/parser/WebIDL.py" ]; then
+  TREE_PYMODULES="/tmp/pymodules-${TREE_NAME}"
+  mkdir -p "${TREE_PYMODULES}"
+  pushd "${TREE_PYMODULES}"
+  cp "${FILES_ROOT}/xpcom/idl-parser/xpidl/xpidl.py" ./
+  cp "${FILES_ROOT}/dom/bindings/parser/WebIDL.py" ./
+  mkdir ply
+  pushd ply
+  for PLYFILE in __init__.py lex.py yacc.py; do
+    cp "${FILES_ROOT}/other-licenses/ply/ply/${PLYFILE}" ./ || cp "${FILES_ROOT}/third_party/python/ply/ply/${PLYFILE}" ./
+  done
+  popd
+  popd
+  export PYTHONPATH="${TREE_PYMODULES}${PYTHONPATH:+:${PYTHONPATH}}"
+fi
+
 cat $INDEX_ROOT/idl-files | \
     parallel $MOZSEARCH_PATH/scripts/idl-analyze.py \
     $INDEX_ROOT $FILES_ROOT/{} ">" $INDEX_ROOT/analysis/{}
