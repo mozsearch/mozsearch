9932a72f0219b1e772d544b8d676a538b98010d0
Tooru Fujisawa <arai_a@mac.com>
2025-11-22 17:04:32 +0900

    Bug 2001790 - Part 1: Use separate target between interface and partial interface.

--- tools/src/file_format/analysis.rs
+++ tools/src/file_format/analysis.rs
@@ -135,6 +135,8 @@ pub enum AnalysisKind {
     Decl,
     Forward,
     Idl,
+    // Partial interface/namespace in IDL.
+    Idlp,
     Alias,
 }
 
@@ -149,6 +151,7 @@ impl AnalysisKind {
             AnalysisKind::Decl => ustr("decl"),
             AnalysisKind::Forward => ustr("forward"),
             AnalysisKind::Idl => ustr("idl"),
+            AnalysisKind::Idlp => ustr("idlp"),
             AnalysisKind::Alias => ustr("alias"),
         }
     }
