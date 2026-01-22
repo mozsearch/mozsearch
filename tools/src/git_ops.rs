b081a0d7f32210af5ad5922953938706e08f4308
Nicolas Guichard <nicolas.guichard@kdab.com>
2026-01-22 10:59:42 +0100

    Bug 2002216 - Display code coverage information in directory view
    This adds a new “coverage” column to the file table, sourced from
    summary data stored in the coverage Git repository.
    
    For the static index generation, this goes through ConcisePerFileInfo.
    For the dynamic /rev endpoint, format_tree diretly queries the coverage
    repository.
    
    Co-authored-by: Aleš Petrovački <ales.petrovacki@kdab.com>

--- tools/src/git_ops.rs
+++ tools/src/git_ops.rs
@@ -2,6 +2,7 @@ use git2::{Commit, Object, Repository, TreeEntry};
 use std::{path::Path, str::FromStr};
 
 use crate::file_format::{
+    code_coverage_report,
     config::GitData,
     coverage::{interpolate_coverage, InterpolatedCoverage},
 };
@@ -77,4 +78,52 @@ pub fn get_coverage(
                     interpolate_coverage(raw)
                 })
         })
+}
+
+/// Returns the coverage summary for the given path, if available.
+/// Note: coverage_rev must be a reference in the coverage repository, not in the main repository.
+pub fn coverage_summary(
+    git_data: Option<&GitData>,
+    coverage_rev: &str,
+    path: impl AsRef<Path>,
+) -> Option<code_coverage_report::NodeMetadata> {
+    let path = path.as_ref();
+
+    let coverage_repo = git_data.as_ref()?.coverage_repo.as_ref()?;
+
+    let coverage_commit = coverage_repo
+        .revparse_single(coverage_rev)
+        .ok()?
+        .peel_to_commit()
+        .ok()?;
+
+    let tree = coverage_commit.tree().ok()?;
+
+    let covered = tree.get_path(path).ok()?;
+
+    let coverage_summary_path = match covered.kind()? {
+        git2::ObjectType::Tree => path.join("index.json"),
+        git2::ObjectType::Blob => path.with_added_extension("summary.json"),
+        _ => return None,
+    };
+
+    let coverage_summary_object = tree
+        .get_path(&coverage_summary_path)
+        .ok()?
+        .to_object(coverage_repo)
+        .ok()?;
+
+    serde_json::from_slice(coverage_summary_object.as_blob()?.content()).ok()
+}
+
+/// Returns the coverage summary for the given path for the HEAD revision of the main repository, if available.
+pub fn coverage_summary_for_head(
+    git_data: Option<&GitData>,
+    path: impl AsRef<Path>,
+) -> Option<code_coverage_report::NodeMetadata> {
+    let head_oid = git_data?.repo.head().ok()?.peel_to_commit().ok()?.id();
+
+    let coverage_rev = format!("refs/tags/reverse/all/all/{}", head_oid);
+
+    coverage_summary(git_data, &coverage_rev, path)
 }
