3ac87231a7da8bf58de7e3a8c37ab94a80a36c5e
Tooru Fujisawa <arai_a@mac.com>
2026-01-23 12:17:42 +0900

    Bug 2011772 - Part 4: Add test.

--- tests/webtest/test_BlamePhab.js
+++ tests/webtest/test_BlamePhab.js
@@ -1,6 +1,6 @@
 "use strict";
 
-add_task(async function test_TreeSwitcherKeyboardNavigation() {
+add_task(async function test_TestPhabricator() {
   await TestUtils.loadPath("/searchfox/source/tests/tests/files/blame/blame-phab.txt");
 
   const blameStrip = frame.contentDocument.querySelector(`#line-1 .blame-strip`);
@@ -14,4 +14,20 @@ add_task(async function test_TreeSwitcherKeyboardNavigation() {
   });
 
   is(link.href, "https://searchfox.org/D12345", "Phabricator revision link equals to the commit message");
+});
+
+add_task(async function test_TestPullRequest() {
+  await TestUtils.loadPath("/searchfox/source/tests/tests/files/blame/blame-pr.txt");
+
+  const blameStrip = frame.contentDocument.querySelector(`#line-1 .blame-strip`);
+
+  TestUtils.dispatchMouseEvent("mouseenter", blameStrip);
+
+  let link;
+  await waitForCondition(() => {
+    link = frame.contentDocument.querySelector(`#blame-popup a[href*="900"]`);
+    return link;
+  });
+
+  is(link.href, "https://github.com/mozsearch/mozsearch/pull/900", "Pull request link equals to the commit message");
 });
