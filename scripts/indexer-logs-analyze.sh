eb572cd67f07a4c165b94c7304dbefcd30c99857
Tooru Fujisawa <arai_a@mac.com>
2026-01-29 23:27:38 +0900

    Bug 2013053 - Part 4: Use step log in indexer-logs-print.py.

--- scripts/indexer-logs-analyze.sh
+++ scripts/indexer-logs-analyze.sh
@@ -2,52 +2,5 @@
 
 SCRIPT_DIR=$(dirname $0)
 
-GREP=grep
-if [ $(uname) == "Darwin" ]; then
-    GREP=ggrep
-    if ! which ${GREP} > /dev/null; then
-        echo "Please install GNU grep with 'brew install grep'"
-        exit 1
-    fi
-fi
-
-PARSE_EXPR='parse "* + /home/ubuntu/mozsearch/scripts/* *" '
-PARSE_EXPR+=' as time, script, args'
-PARSE_EXPR+=' | parseDate(time) as time'
-PARSE_EXPR+=' | split(args) on " "'
-PARSE_EXPR+=' | split(args[0]) on "/" as args0'
-# scripts where the 3rd argument is the tree name and we can use it as-is
-PARSE_EXPR+=' | if(script == "find-repo-files.py" or script == "build.sh" or script=="output.sh", args[2], "") as tree'
-# scripts where the 2nd argument is the tree name and we can use it as-is
-PARSE_EXPR+=' | if(script == "js-analyze.sh" or script == "java-analyze.sh" or'
-PARSE_EXPR+=' script == "scip-analyze.sh" or script == "idl-analyze.sh" or'
-PARSE_EXPR+=' script == "ipdl-analyze.sh" or script == "crossref.sh" or'
-PARSE_EXPR+=' script == "build-codesearch.py" or script == "check-index.sh" or'
-PARSE_EXPR+=' script == "compress-outputs.sh" or script == "check-index.sh" or'
-PARSE_EXPR+=' script == "html-analyze.sh" or script == "css-analyze.sh" or'
-PARSE_EXPR+=' script == "staticprefs-analyze.sh", args[1], tree) as tree'
-# scripts where the 1st argument has a path segment which is the tree name we
-# can use.  We split the first argument above to be `args0` for this.
-PARSE_EXPR+=' | if(script == "process-chrome-map.py" or script == "replace-aliases.sh", args0[2], tree) as tree'
-
-# - Grep the log in Perl mode looking for the pattern where a "date" invocation
-#   is followed by a script invocation from mozsearch/scripts.
-#   - We use `-P` to get fancy Perl mode
-#   - We use `-a` to force ASCII mode so it doesn't decide it's a binary file.
-#   - We use `-z` so that grep sees a single giant line, which combined with
-#     `-o` only outputs what matched.  We use a look-behind assertion so that
-#     we can match on the `+ date` line but not include it in the output.
-#   - We use `-h` to suppress the filename
-# - We use `paste` to join these consecutive lines.
-# - We use `tr -d '\0'` to eat a leading nul that ends up in there at the start
-#   of the lines.
-#
-# The net output looks like:
-# Sat Oct  2 04:41:33 UTC 2021 + /home/ubuntu/mozsearch/scripts/find-repo-files.py /home/ubuntu/config /mnt/index-scratch/config.json nss
-# Sat Oct  2 04:41:35 UTC 2021 + /home/ubuntu/mozsearch/scripts/build.sh /home/ubuntu/config /mnt/index-scratch/config.json nss
-# Sat Oct  2 04:41:35 UTC 2021 + /home/ubuntu/mozsearch/scripts/indexer-setup.py
-${GREP} -Pazoh "(?<=\n\+ date\n)[^\n]+\n\+ /home/ubuntu/mozsearch/scripts/[^\n]+\n" index-* \
-  | paste -d" " - - \
-  | tr -d '\0' \
-  | agrind --output json "* | ${PARSE_EXPR}" \
+grep "^Performing .* step for" index-* \
   | ${SCRIPT_DIR}/indexer-logs-print.py
--- scripts/mkindex.sh
+++ scripts/mkindex.sh
@@ -81,10 +81,6 @@ export RUST_BACKTRACE=1
 #     SKIP=compress-outputs make review-test-repo
 #     and then
 #     SKIP=mkdirs,build,compress-outputs make review-test-repo
-#
-# Each step should have `date` command, directly followed by the
-# invocation for the script inside the scripts directory.
-# indexer-logs-analyze.sh expects that order.
 should_perform() {
     if [[ ${SKIP:-} != "" ]]; then
         if echo ",$SKIP," | grep ",$1," > /dev/null; then
@@ -102,12 +98,11 @@ should_perform() {
         return 1
     fi
 
-    echo "Performing $1 step for $TREE_NAME : $(date)"
+    echo "Performing $1 step for $TREE_NAME : $(date +"%Y-%m-%dT%H:%M:%S%z")"
     return 0
 }
 
 if should_perform "find-repo-files"; then
-    date
     $MOZSEARCH_PATH/scripts/find-repo-files.py $CONFIG_REPO $CONFIG_FILE $TREE_NAME
 fi
 
@@ -115,7 +110,6 @@ fi
 if should_perform "mkdirs"; then
     # NOTE: This step removes all intermediate files/directories from the
     #       previous run.
-    date
     $MOZSEARCH_PATH/scripts/mkdirs.sh
 else
     SKIPPED_MKDIRS=1
@@ -126,12 +120,10 @@ if [[ ${SKIPPED_MKDIRS:-} -eq 1 ]]; then
     #       are reused from the previous run.
     #       undo the compress-outputs step, just in case the previous run
     #       didn't skip the compress-outputs step.
-    date
     $MOZSEARCH_PATH/scripts/uncompress-outputs.sh $CONFIG_FILE $TREE_NAME || handle_tree_error "uncompress-outputs.sh"
 fi
 
 if should_perform "build"; then
-    date
     $MOZSEARCH_PATH/scripts/build.sh $CONFIG_REPO $CONFIG_FILE $TREE_NAME
 fi
 
@@ -145,19 +137,16 @@ if should_perform "scip-analyze"; then
     # merge-analyses may be required, that the scripts will handle calling
     # `scip-analyze.sh` or `scip-indexer` directly and will not list them in
     # `scip_subtrees`.
-    date
     $MOZSEARCH_PATH/scripts/scip-analyze.sh \
         "$CONFIG_FILE" \
         "$TREE_NAME" || handle_tree_error "scip-analyze.sh"
 fi
 
 if should_perform "find-objdir-files"; then
-    date
     $MOZSEARCH_PATH/scripts/find-objdir-files.sh
 fi
 
 if should_perform "objdir-mkdirs"; then
-    date
     $MOZSEARCH_PATH/scripts/objdir-mkdirs.sh
 fi
 
@@ -165,85 +154,69 @@ URL_MAP_PATH=$INDEX_ROOT/aliases/url-map.json
 DOC_TREES_MAP=$INDEX_ROOT/doc-trees.json
 
 if should_perform "process-chrome-map"; then
-    date
     $MOZSEARCH_PATH/scripts/process-chrome-map.py $GIT_ROOT $URL_MAP_PATH $INDEX_ROOT/*.chrome-map.json || handle_tree_error "process-chrome-map.py"
 fi
 
 if should_perform "js-analyze"; then
-    date
     $MOZSEARCH_PATH/scripts/js-analyze.sh $CONFIG_FILE $TREE_NAME || handle_tree_error "js-analyze.sh"
 fi
 
 if should_perform "html-analyze"; then
-    date
     $MOZSEARCH_PATH/scripts/html-analyze.sh $CONFIG_FILE $TREE_NAME || handle_tree_error "html-analyze.sh"
 fi
 
 if should_perform "css-analyze"; then
-    date
     $MOZSEARCH_PATH/scripts/css-analyze.sh $CONFIG_FILE $TREE_NAME || handle_tree_error "css-analyze.sh"
 fi
 
 if should_perform "idl-analyze"; then
-    date
     $MOZSEARCH_PATH/scripts/idl-analyze.sh $CONFIG_FILE $TREE_NAME || handle_tree_error "idl-analyze.sh"
 fi
 
 if should_perform "staticprefs-analyze"; then
-    date
     $MOZSEARCH_PATH/scripts/staticprefs-analyze.sh $CONFIG_FILE $TREE_NAME || handle_tree_error "idl-analyze.sh"
 fi
 
 if should_perform "ipdl-analyze"; then
-    date
     $MOZSEARCH_PATH/scripts/ipdl-analyze.sh $CONFIG_FILE $TREE_NAME || handle_tree_error "ipdl-analyze.sh"
 fi
 
 if should_perform "toml-analyze"; then
-    date
     $MOZSEARCH_PATH/scripts/toml-analyze.sh $CONFIG_FILE $TREE_NAME || handle_tree_error "toml-analyze.sh"
 fi
 
 if should_perform "yaml-analyze"; then
-    date
     $MOZSEARCH_PATH/scripts/yaml-analyze.sh $CONFIG_FILE $TREE_NAME || handle_tree_error "yaml-analyze.sh"
 fi
 
 if should_perform "mozbuild-analyze"; then
-    date
     $MOZSEARCH_PATH/scripts/mozbuild-analyze.sh $CONFIG_FILE $TREE_NAME || handle_tree_error "mozbuild-analyze.sh"
 fi
 
 ANALYSIS_FILES_PATH=$INDEX_ROOT/all-analysis-files
 
 if should_perform "generate-analsysis-files-list"; then
-    date
     $MOZSEARCH_PATH/scripts/generate-analsysis-files-list.sh $ANALYSIS_FILES_PATH || handle_tree_error "generate-analsysis-files-list.sh"
 fi
 
 if should_perform "replace-aliases"; then
-    date
     $MOZSEARCH_PATH/scripts/replace-aliases.sh $ANALYSIS_FILES_PATH || handle_tree_error "replace-aliases.sh"
 fi
 
 if should_perform "annotate-gc"; then
-    date
     $MOZSEARCH_PATH/scripts/annotate-gc.sh $ANALYSIS_FILES_PATH || handle_tree_error "annotate-gc.sh"
 fi
 
 if should_perform "crossref"; then
     # crossref failures always need to be fatal because their outputs are required.
-    date
     $MOZSEARCH_PATH/scripts/crossref.sh $CONFIG_FILE $TREE_NAME $ANALYSIS_FILES_PATH
 fi
 
 if should_perform "output"; then
-    date
     $MOZSEARCH_PATH/scripts/output.sh $CONFIG_REPO $CONFIG_FILE $TREE_NAME $URL_MAP_PATH $DOC_TREES_MAP || handle_tree_error "output.sh"
 fi
 
 if should_perform "build-codesearch"; then
-    date
     $MOZSEARCH_PATH/scripts/build-codesearch.py $CONFIG_FILE $TREE_NAME || handle_tree_error "build-codesearch.py"
 fi
 
@@ -251,7 +224,6 @@ if should_perform "compress-outputs"; then
     # This depends on INDEX_ROOT already being available.  The script doesn't
     # actually care about CONFIG_FILE or TREE_NAME, but it's helpful to
     # `indexer-logs-analyze.sh`.
-    date
     $MOZSEARCH_PATH/scripts/compress-outputs.sh $CONFIG_FILE $TREE_NAME || handle_tree_error "compress-outputs.sh"
 fi
 
@@ -259,12 +231,10 @@ if should_perform "codesearch-start"; then
     # Check the resulting index for correctness, but there's no webserver so the
     # 4th argument needs to be empty.  We now also need the livegrep server to be
     # available, so start that first.
-    date
     $MOZSEARCH_PATH/router/codesearch.py $CONFIG_FILE start $TREE_NAME
 fi
 
 if should_perform "check-index"; then
-    date
     $MOZSEARCH_PATH/scripts/check-index.sh $CONFIG_FILE $TREE_NAME "filesystem" ""
 fi
 
@@ -275,5 +245,3 @@ if should_perform "codesearch-stop"; then
     # indexing process.
     $MOZSEARCH_PATH/router/codesearch.py $CONFIG_FILE stop $TREE_NAME
 fi
-
-date
