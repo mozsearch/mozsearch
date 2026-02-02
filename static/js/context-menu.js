3f62bdab4e1edc107ae9b4894ba7c1f476087e50
Tooru Fujisawa <arai_a@mac.com>
2026-02-02 17:21:48 +0900

    Bug 2013578 - Part 11: Show context menu item for both exported symbol and module global.

--- static/js/context-menu.js
+++ static/js/context-menu.js
@@ -1242,6 +1242,7 @@ var ContextMenu = new (class ContextMenu extends ContextMenuOrSubMenu {
       let jsLocalSyms = [];
       let nonJSLocalSyms = [];
       let sawDef = false;
+      let hasModuleGlobalAndExported = false;
       symbols.forEach((sym, index) => {
         // Avoid processing the same symbol more than once.
         if (seenSyms.has(sym)) {
@@ -1312,7 +1313,12 @@ var ContextMenu = new (class ContextMenu extends ContextMenuOrSubMenu {
 
         if (symInfo?.jumps?.idl === sourceLineClicked ||
             symInfo?.jumps?.def === sourceLineClicked ) {
-          if (!sawDef) {
+          if (sym.startsWith("#") &&
+              filteredSymTuples.length == 1 &&
+              filteredSymTuples[0][0] == "#M-" + sym.slice(1)) {
+            hasModuleGlobalAndExported = true;
+          }
+          if (!sawDef && !hasModuleGlobalAndExported) {
             // Transition to "kick out the implicit constructors" mode.
             sawDef = true;
             filteredSymTuples = [];
@@ -1508,6 +1514,13 @@ var ContextMenu = new (class ContextMenu extends ContextMenuOrSubMenu {
               def: symInfo?.jumps?.def,
             });
           }
+        } else if (hasModuleGlobalAndExported && sym.startsWith("#M-")) {
+          jumpify(symInfo, symInfo.pretty);
+          searches.push({
+            label: "module global " + symInfo.pretty,
+            syms: [sym],
+            def: symInfo?.jumps?.def,
+          });
         } else {
           jumpify(symInfo, symInfo.pretty);
           searches.push({
