0527f61b81aa855951b2f75affa3916c0f0095e7
Tooru Fujisawa <arai_a@mac.com>
2026-02-07 19:45:47 +0900

    Bug 2009919 - Part 1: Emit structured record for the template functions.

--- clang-plugin/MozsearchIndexer.cpp
+++ clang-plugin/MozsearchIndexer.cpp
@@ -2240,7 +2240,6 @@ public:
     int Flags = 0;
     const char *Kind = "def";
     const char *PrettyKind = "?";
-    bool wasTemplate = false;
     SourceRange PeekRange(D->getBeginLoc(), D->getEndLoc());
     // The nesting range identifies the left brace and right brace, which
     // heavily depends on the AST node type.
@@ -2248,7 +2247,6 @@ public:
     QualType qtype = QualType();
     if (FunctionDecl *D2 = dyn_cast<FunctionDecl>(D)) {
       if (D2->isTemplateInstantiation()) {
-        wasTemplate = true;
         D = D2->getTemplateInstantiationPattern();
       }
       // We treat pure virtual declarations as definitions.
@@ -2397,11 +2395,7 @@ public:
       }
     }
     if (FunctionDecl *D2 = dyn_cast<FunctionDecl>(D)) {
-      if ((D2->isThisDeclarationADefinition() || isPure(D2)) &&
-          // a clause at the top should have generalized and set wasTemplate so
-          // it shouldn't be the case that isTemplateInstantiation() is true.
-          !D2->isTemplateInstantiation() && !wasTemplate &&
-          !D2->isFunctionTemplateSpecialization() && !TemplateStack) {
+      if (D2->isThisDeclarationADefinition() || isPure(D2)) {
         if (auto *D3 = dyn_cast<CXXMethodDecl>(D2)) {
           findBindingToJavaMember(*AstContext, *D3);
         } else {
