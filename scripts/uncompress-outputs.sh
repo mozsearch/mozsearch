1f4fd4aa80a49d13b357429bb7ba0ee16376cfa2
Tooru Fujisawa <arai_a@mac.com>
2026-02-10 13:35:46 +0900

    Bug 2015792 - Parallelize uncompress-outputs.sh.

--- scripts/uncompress-outputs.sh
+++ scripts/uncompress-outputs.sh
@@ -7,12 +7,8 @@ set -o pipefail # Check all commands in a pipeline
 function uncompress_dir {
   echo "Uncompressing files in $1"
   pushd $1
-  find . -type f -name '*.gz' | while read GZ_FILE; do
-      RAW_FILE=$(echo "$GZ_FILE" | sed -e 's/\.gz$//')
-      if [[ -f "$RAW_FILE" ]]; then
-          gunzip -f "$GZ_FILE"
-      fi
-  done
+  find . -type f -name '*.gz' | sed -e 's/\.gz$//' | \
+      parallel --halt now,fail=1 'if [[ -f {} ]]; then gunzip -f {}.gz; fi'
   popd
 }
 
