c3391e0ada3eac0187014a4e4e879ea1f1da66af
Tooru Fujisawa <arai_a@mac.com>
2026-02-21 11:01:16 +0900

    Bug 2018372 - Parallelize annotate-gc.sh.

--- scripts/annotate-gc.sh
+++ scripts/annotate-gc.sh
@@ -7,5 +7,9 @@ set -o pipefail # Check all commands in a pipeline
 ANALYSIS_FILES_PATH=$1
 
 if [ -f $INDEX_ROOT/gcFunctions.txt -a -f $INDEX_ROOT/allFunctions.txt ]; then
-    cat $ANALYSIS_FILES_PATH | $MOZSEARCH_PATH/scripts/annotate-gc.py $INDEX_ROOT/analysis $INDEX_ROOT/gcFunctions.txt $INDEX_ROOT/allFunctions.txt
+    cat $ANALYSIS_FILES_PATH | \
+        parallel --jobs 16 --pipe --halt 2 \
+        $MOZSEARCH_PATH/scripts/annotate-gc.py \
+        $INDEX_ROOT/analysis \
+        $INDEX_ROOT/gcFunctions.txt $INDEX_ROOT/allFunctions.txt
 fi
