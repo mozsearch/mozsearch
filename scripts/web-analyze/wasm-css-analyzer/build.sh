960814099600dc7b69e94359c8d9c81c02c58efe
Nicolas Guichard <nicolas.guichard@kdab.com>
2026-03-13 13:37:01 +0100

    Remove install of wasm-pack and wasm-snip in wasm-css-analyzer build script
    Those are leftovers, these utilities are installed in the provisioning
    step.

--- scripts/web-analyze/wasm-css-analyzer/build.sh
+++ scripts/web-analyze/wasm-css-analyzer/build.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-if ! which wasm-pack > /dev/null; then
-    cargo install wasm-pack
-fi
-
-if ! which wasm-snip > /dev/null; then
-    cargo install wasm-snip
-fi
-
 wasm-pack build --release --target web
 
 mkdir -p out
